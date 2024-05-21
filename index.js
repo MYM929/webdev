f191d986822c651d22089eba8dcb29675b6e5554
mym <maiyongming929@gmail.com>
2024-05-21 17:18:30 -0500

    js #4: Accept user input

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-let result = 2 ** 3;
-console.log(result);
+// let username;
+// username = window.prompt("What is your username");
+// console.log(username);
+
+let username;
+
+document.getElementById("mySubmit").onclick = function () {
+  username = document.getElementById("myText").value;
+  //console.log(username);
+  document.getElementById("myH1").textContent = `Hello ${username}`;
+};
