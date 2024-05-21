a2b3153df99807f4ebea37e7054e6a7418681cfe
mym <maiyongming929@gmail.com>
2024-05-21 15:12:00 -0500

    js #3: Arithmetic operators

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-let fullName = "Yongming Mai";
-let age = 19;
-let isStudent = true;
-
-document.getElementById("p1").textContent = `Your name is ${fullName}`;
-document.getElementById("p2").textContent = `You are ${age} years old`;
-document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+let result = 2 ** 3;
+console.log(result);
