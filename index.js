10f0dc1d9a934e284be4447a3c2efd716e2357fe
mym <maiyongming929@gmail.com>
2024-05-22 00:22:02 -0500

    js #8: Math object

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const countLabel = document.getElementById("countLabel");
+console.log(Math.PI);
+console.log(Math.E);
 
-const decreaseButton = document.getElementById("decreaseButton");
-const resetButton = document.getElementById("resetButton");
-const increaseButton = document.getElementById("increaseButton");
+let x = 3.21;
+let y = 2;
+let z;
 
-let count = 0;
+//z = Math.round(x);
+//z = Math.floor(x);
+//z = Math.ceil(x);
+//z = Math.trunc(x);
+//z = Math.pow(x, y);
+//z = Math.sqrt(x);
+//z = Math.log(x);
+//z = Math.sin(x); //rad
+//z = Math.abs(x);
+//z = Math.sign(x);
 
-increaseButton.onclick = function () {
-  count++;
-  countLabel.textContent = count;
-};
+let max = Math.max(x, y);
+//let min = Math.min(x, y);
 
-decreaseButton.onclick = function () {
-  count--;
-  countLabel.textContent = count;
-};
-
-resetButton.onclick = function () {
-  count = 0;
-  countLabel.textContent = count;
-};
+console.log(max);
