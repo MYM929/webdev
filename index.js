c907cd3a09dc917df830490de409f75925858eaa
mym <maiyongming929@gmail.com>
2024-05-22 12:16:17 -0500

    js #9: Random number generator

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-console.log(Math.PI);
-console.log(Math.E);
+const myButton = document.getElementById("myButton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
 
-let x = 3.21;
-let y = 2;
-let z;
+const min = 1;
+const max = 6;
 
-//z = Math.round(x);
-//z = Math.floor(x);
-//z = Math.ceil(x);
-//z = Math.trunc(x);
-//z = Math.pow(x, y);
-//z = Math.sqrt(x);
-//z = Math.log(x);
-//z = Math.sin(x); //rad
-//z = Math.abs(x);
-//z = Math.sign(x);
+let randomNum1;
+let randomNum2;
+let randomNum3;
 
-let max = Math.max(x, y);
-//let min = Math.min(x, y);
+myButton.onclick = function () {
+  randomNum1 = Math.floor(Math.random() * max) + min;
+  randomNum2 = Math.floor(Math.random() * max) + min;
+  randomNum3 = Math.floor(Math.random() * max) + min;
 
-console.log(max);
+  label1.textContent = randomNum1;
+  label2.textContent = randomNum2;
+  label3.textContent = randomNum3;
+};
