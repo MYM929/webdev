156e947421d61d60aa79e2fb04520db965b5a296
mym <maiyongming929@gmail.com>
2024-05-21 22:40:55 -0500

    js #5: Type conversion

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-// let username;
-// username = window.prompt("What is your username");
-// console.log(username);
+let x = "0";
+let y = "0";
+let z = "0";
 
-let username;
+x = Number(x);
+y = String(y);
+z = Boolean(z);
 
-document.getElementById("mySubmit").onclick = function () {
-  username = document.getElementById("myText").value;
-  //console.log(username);
-  document.getElementById("myH1").textContent = `Hello ${username}`;
-};
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+/*
+x = "pizza"
+y = "pizza"
+z = "pizza"
+
+NaN 'number'
+pizza string
+true 'boolean'
+
+x = "0"
+y = "0"
+z = "0"
+
+0 'number'
+0 string
+true 'boolean'
+
+x = ""
+y = ""
+z = ""
+
+0 'number'
+string
+false 'boolean'
+
+x
+y
+z
+
+NaN 'number'
+undefined string
+false 'boolean'
+*/
