2ece710569667bed70f32ae1961380dccfaddff0
mym <maiyongming929@gmail.com>
2024-05-22 23:54:28 -0500

    js #13: Switches

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
-let age = 12;
-let message = age >= 18 ? "You're an adult" : "You're a minor";
-console.log(message);
+// let day = 1;
+
+// switch (day) {
+//   case 1:
+//     console.log("It is Monday");
+//     break;
+//   case 2:
+//     console.log("It is Tueday");
+//     break;
+//   case 3:
+//     console.log("It is Wednesday");
+//     break;
+//   case 4:
+//     console.log("It is Thursday");
+//     break;
+//   case 5:
+//     console.log("It is Friday");
+//     break;
+//   case 6:
+//     console.log("It is Saturday");
+//     break;
+//   case 7:
+//     console.log("It is Sunday");
+//     break;
+//   default:
+//     console.log(`${day} is not a day`);
+//     break;
+// }
+
+let testScore = 33;
+let letterGrade;
+
+switch (true) {
+  case testScore >= 90:
+    letterGrade = "A";
+    break;
+  case testScore >= 80:
+    letterGrade = "B";
+    break;
+  case testScore >= 70:
+    letterGrade = "C";
+    break;
+  case testScore >= 60:
+    letterGrade = "D";
+    break;
+  default:
+    letterGrade = "F";
+    break;
+}
+
+console.log(letterGrade);
