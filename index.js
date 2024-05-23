d400c2b8e929c05494bae7062cd0ec1d6ef6d879
mym <maiyongming929@gmail.com>
2024-05-22 23:46:17 -0500

    js #12: Ternary operator

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-const myCheckbox = document.getElementById("myCheckbox");
-const visaButton = document.getElementById("visaButton");
-const masterCardButton = document.getElementById("masterCardButton");
-const paypalButton = document.getElementById("paypalButton");
-const mySubmit = document.getElementById("mySubmit");
-const subResult = document.getElementById("subResult");
-const paymentResult = document.getElementById("paymentResult");
-
-mySubmit.onclick = function () {
-  if (myCheckbox.checked) {
-    subResult.textContent = `You are subscribed`;
-  } else {
-    subResult.textContent = `You are not subscribed`;
-  }
-
-  if (visaButton.checked) {
-    paymentResult.textContent = `You are paying with Visa`;
-  } else if (masterCardButton.checked) {
-    paymentResult.textContent = `You are paying with MasterCard`;
-  } else if (paypalButton.checked) {
-    paymentResult.textContent = `You are paying with Paypal`;
-  } else {
-    paymentResult.textContent = `You must select a payment type`;
-  }
-};
+let age = 12;
+let message = age >= 18 ? "You're an adult" : "You're a minor";
+console.log(message);
