1495d4ab407a21238da3b7c1a0bf746941c293a0
mym <maiyongming929@gmail.com>
2024-05-23 19:30:08 -0500

    js #20: For loops

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-let loggedIn = false;
-let username;
-let password;
-
-while (!loggedIn) {
-  username = window.prompt(`Enter your username`);
-  password = window.prompt(`Enter your password`);
-
-  if (username === "myUsername" && password === "myPassword") {
-    loggedIn = true;
-    console.log("You are logged in!");
+for (let i = 1; i <= 20; i++) {
+  if (i == 13) {
+    break; //continue
   } else {
-    console.log("Invalid credentials! Please try again");
+    console.log(i);
   }
 }
