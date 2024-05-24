c30fe6769b24639fbc484b08f60ed4fa3ddbf62b
mym <maiyongming929@gmail.com>
2024-05-23 23:17:02 -0500

    js #24: emperature conversion program

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-let x = 3;
+const textBox = document.getElementById("textBox");
+const toF = document.getElementById("toF");
+const toC = document.getElementById("toC");
+const result = document.getElementById("result");
 
-function1();
-function2();
+let temp;
 
-function function1()
+function convert()
 {
-  let x = 1;
-  console.log(x);
-}
-function function2()
-{
-  let x = 2;
-  console.log(x);
+  if(toF.checked)
+  {
+    temp = Number(textBox.value);
+    temp = temp*9/5 + 32;
+    result.textContent = temp.toFixed(1) + " F";
+  }
+  else if(toC.checked)
+  {
+    temp = Number(textBox.value);
+    temp = (temp-32)*(5/9);
+    result.textContent = temp.toFixed(1) + " C";
+  }
+  else
+  {
+    result.textContent = "Select a unit";
+  }
 }
