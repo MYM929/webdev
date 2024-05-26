8d7bcc4acfdeb8a09b77b97471b52d7f6b82df60
mym <maiyongming929@gmail.com>
2024-05-26 18:20:47 -0500

    js #50: Dates

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const cards = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
+const date1 = new Date("2023-12-31");
+const date2 = new Date("2024-01-01");
 
-//Fisher-Yates algorithm
-function shuffle(array){
-    for(let i=array.length-1; i>0; i--){
-        const random = Math.floor(Math.random()*(i+1));
-        [array[i], array[random]] = [array[random], array[i]];
-    }
+if(date2 > date1){
+    console.log("HAPPY NEW YEAR!");
 }
 
-shuffle(cards);
-console.log(cards);
+
+
+/* 
+// Date(year, monty, day, hour, minute, second, ms)
+
+const date = new Date();
+
+// const date = new Date(2024, 0, 1, 2, 3, 4, 5);
+// const date = new Date("2024-01-02T12:00:00Z");
+// const date = new Date(1700000000000); //unit: ms
+
+// date.setFullYear(2024);
+
+console.log(date)
+
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDate();
+// const dayOfWeek = date.getDay();
+
+// console.log(year);
+// console.log(month);
+// console.log(day);
+// console.log(dayOfWeek); 
+*/
