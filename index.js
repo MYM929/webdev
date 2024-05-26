2e1abbb49ef1790f35b65db62bc3f6702c7fcb51
mym <maiyongming929@gmail.com>
2024-05-26 00:04:31 -0500

    js #36: Arrow functions

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const numbers = [1,2,3,4,5,6];
 
-const squares = numbers.map(function(element){
-  return Math.pow(element, 2);
-});
-const evenNums = numbers.filter(function(element){
-  return element%2 === 0;
-});
-const total = numbers.reduce(function(accumulator, element){
-  return accumulator + element;
-});
+const squares = numbers.map((element) => Math.pow(element, 2));
+const evenNums = numbers.filter((element) => element%2 === 0);
+const total = numbers.reduce((accumulator, element) => accumulator + element);
 
 console.log(squares);
 console.log(evenNums);
@@ -18,16 +12,19 @@ console.log(total);
 
 
 
-/* 
-setTimeout(function () {
-  console.log("Hello");
-}, 3000); 
+/*  
+setTimeout(() => console.log("Hello"), 3000); 
 */
 
 /* 
-const hello = function(){
-  console.log("Hello");
-}
+const hello = (name,age) => {console.log(`Hello ${name}`)
+                             console.log(`You are ${age} years old`)};
+
+hello("Bro", 25); 
+*/
+
+/* 
+const hello = () => console.log("Hello");
 
 setTimeout(hello, 3000);
 */
