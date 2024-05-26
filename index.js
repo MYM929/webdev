47f7ca982a0785271f2e918a33c072c36f7affd1
mym <maiyongming929@gmail.com>
2024-05-26 18:10:38 -0500

    js #49: Shuffle an array

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-const people = [{name: "aaa", age: 30, gpa: 3.0},
-                {name: "bbb", age: 37, gpa: 1.5},
-                {name: "ccc", age: 51, gpa: 2.5},
-                {name: "ddd", age: 27, gpa: 4.0},];
+const cards = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
 
-people.sort((a,b) => a.age - b.age);
-console.log(people);
+//Fisher-Yates algorithm
+function shuffle(array){
+    for(let i=array.length-1; i>0; i--){
+        const random = Math.floor(Math.random()*(i+1));
+        [array[i], array[random]] = [array[random], array[i]];
+    }
+}
 
-people.sort((a,b) => a.name.localeCompare(b.name));
-console.log(people);
-
-
-/* 
-let fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
-let numbers = [1,10,2,9,3,8,4,7,5,6];
-
-fruits.sort();
-numbers.sort((a, b) => a-b);
-numbers.sort((a, b) => b-a); //reverse
-
-console.log(fruits);
-console.log(numbers);
-*/
+shuffle(cards);
+console.log(cards);
