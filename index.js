4485e1fc7120f2ff28ba5fb3572f7b63e19ae646
mym <maiyongming929@gmail.com>
2024-05-25 23:53:35 -0500

    js #35: Function expressions

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-const grades = [75,50,90,80,65,95];
-const max = grades.reduce(getMax);
-console.log(max);
-const min = grades.reduce(getMin);
-console.log(min);
+const numbers = [1,2,3,4,5,6];
 
-function getMax(accumulator, element)
-{
-  return Math.max(accumulator, element);
-}
-function getMin(accumulator, element)
-{
-  return Math.min(accumulator, element);
-}
+const squares = numbers.map(function(element){
+  return Math.pow(element, 2);
+});
+const evenNums = numbers.filter(function(element){
+  return element%2 === 0;
+});
+const total = numbers.reduce(function(accumulator, element){
+  return accumulator + element;
+});
+
+console.log(squares);
+console.log(evenNums);
+console.log(total);
+
+
+
+
 
 /* 
-const prices = [5,30,10,25,15,20];
-const total = prices.reduce(sum);
-console.log(`$${total.toFixed(2)}`);
+setTimeout(function () {
+  console.log("Hello");
+}, 3000); 
+*/
 
-function sum(accumulator, element)
-{
-  return accumulator + element;
-} 
+/* 
+const hello = function(){
+  console.log("Hello");
+}
+
+setTimeout(hello, 3000);
 */
