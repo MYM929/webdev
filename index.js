63f69208f2c1e5429b0c5c915be434fcd68913f0
mym <maiyongming929@gmail.com>
2024-05-25 23:41:37 -0500

    js #34: reduce()

--- index.js
+++ index.js
@@ -1,53 +1,25 @@
-const words = ["apple", "orange", "banana", "kiwi",
-               "pomegrante", "coconut"];
+const grades = [75,50,90,80,65,95];
+const max = grades.reduce(getMax);
+console.log(max);
+const min = grades.reduce(getMin);
+console.log(min);
 
-function getShortWords(element)
+function getMax(accumulator, element)
 {
-  return element.length <= 6;
+  return Math.max(accumulator, element);
 }
-function getLongWords(element)
+function getMin(accumulator, element)
 {
-  return element.length > 6;
+  return Math.min(accumulator, element);
 }
-
-const shortWords = words.filter(getShortWords);
-console.log(shortWords);
-const longWords = words.filter(getLongWords);
-console.log(longWords);
-
 
 /* 
-const ages = [16,17,18,18,19,20,60];
+const prices = [5,30,10,25,15,20];
+const total = prices.reduce(sum);
+console.log(`$${total.toFixed(2)}`);
 
-function isAdult(element)
+function sum(accumulator, element)
 {
-  return element >= 18;
-}
-function isChild(element)
-{
-  return element < 18;
-}
-
-const adults = ages.filter(isAdult);
-console.log(adults);
-const children = ages.filter(isChild);
-console.log(children);
-*/
-
-/* 
-let numbers = [1,2,3,4,5,6,7];
-
-function isEven(element)
-{
-  return element % 2 === 0; 
-}
-function isOdd(element)
-{
-  return element % 2 !== 0; 
-}
-
-let evenNums = numbers.filter(isEven);
-console.log(evenNums);
-let oddNums = numbers.filter(isOdd);
-console.log(oddNums); 
+  return accumulator + element;
+} 
 */
