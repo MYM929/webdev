f25a478bc3ffbac0568fc556a46fe44a7a64ae3e
mym <maiyongming929@gmail.com>
2024-05-26 18:35:18 -0500

    js #51: Closures

--- index.js
+++ index.js
@@ -1,32 +1,67 @@
-const date1 = new Date("2023-12-31");
-const date2 = new Date("2024-01-01");
+function createGame(){
+    let score = 0;
 
-if(date2 > date1){
-    console.log("HAPPY NEW YEAR!");
+    function increaseScore(points){
+        score += points;
+        console.log(`+${points}pts`);
+    }
+    function decreaseScore(points){
+        score -= points;
+        console.log(`-${points}pts`);
+    }
+    function getScore(){
+        return score;
+    }
+
+    return {increaseScore, decreaseScore, getScore};
 }
+
+
+const game = createGame();
+
+
+game.increaseScore(5);
+game.increaseScore(6);
+game.decreaseScore(3);
+
+console.log(`The final score is ${game.getScore()}`);
+
+
 
 
 
 /* 
-// Date(year, monty, day, hour, minute, second, ms)
+function createCounter(){
+    let count = 0;
+    function increment(){
+        count++;
+        console.log(`Count increased to ${count}`);
+    }
+    function getCount(){
+        return count;
+    }
 
-const date = new Date();
+    return {increment, getCount};
+}
 
-// const date = new Date(2024, 0, 1, 2, 3, 4, 5);
-// const date = new Date("2024-01-02T12:00:00Z");
-// const date = new Date(1700000000000); //unit: ms
+const counter = createCounter();
+counter.increment(); 
+counter.increment(); 
 
-// date.setFullYear(2024);
+console.log(`The current count is ${counter.getCount()}`); 
+*/
 
-console.log(date)
 
-// const year = date.getFullYear();
-// const month = date.getMonth();
-// const day = date.getDate();
-// const dayOfWeek = date.getDay();
+/* 
+function outer(){
+    let message = "hello";
 
-// console.log(year);
-// console.log(month);
-// console.log(day);
-// console.log(dayOfWeek); 
+    function inner(){
+        console.log(message);
+    }
+    
+    inner();
+}
+
+outer(); 
 */
