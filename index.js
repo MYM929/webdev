4cf8cfe6b3bc339df527a4a4c8aa01e6f39a957d
mym <maiyongming929@gmail.com>
2024-05-26 00:47:09 -0500

    js #40: Classes

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-function Car(make, model, year, color)
-{
-  this.make = make,
-  this.model = model,
-  this.year = year,
-  this.color = color,
-  this.drive = function(){console.log(`You drive the ${this.model}`)}
+class Product{
+  constructor(name, price){
+    this.name = name;
+    this.price = price;
+  }
+
+  displayProduct(){
+    console.log(`Product: ${this.name}`);
+    console.log(`Price: $${this.price.toFixed(2)}`);
+  }
+
+  calculateTotal(salesTax){
+    return this.price + (this.price*salesTax)
+  }
 }
 
-const car1 = new Car("Ford", "Mustang", 2024, "red");
-const car2 = new Car("Ford2", "Mustang", 2024, "red");
+const salesTax = 0.05;
 
-console.log(car1.make);
-console.log(car2.make);
-car1.drive();
+const product1 = new Product("Shirt", 19.99);
+const product2 = new Product("Pants", 22.50);
+const product3 = new Product("Underwear", 100.00);
+
+product1.displayProduct();
+
+const total = product1.calculateTotal(salesTax);
+console.log(`Total price (with tax): $${total.toFixed(2)}`);
