73be9995328678305141f201ab2ba832130639d5
mym <maiyongming929@gmail.com>
2024-05-26 12:18:39 -0500

    js #44: Getters & Setters

--- index.js
+++ index.js
@@ -1,42 +1,97 @@
-class Animal{
-  constructor(name, age){
-    this.name = name;
+class Person{
+  constructor(firstName, lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.age = age;
   }
-  move(speed){
-    console.log(`The ${this.name} moves at a speed of ${speed} mph`);
+
+  set firstName(newFirstname){
+    if(typeof newFirstname === "string" && newFirstname.length>0){
+      this._firstName = newFirstname;
+    }
+    else{
+      console.error("First name must be a non-empty string");
+    }
+  }
+  set lastName(newLastname){
+    if(typeof newLastname === "string" && newLastname.length>0){
+      this._lastname = newLastname;
+    }
+    else{
+      console.error("Last name must be a non-empty string");
+    }
+  }
+  set age(newAge){
+    if(typeof newAge === "number" && newAge>=0){
+      this._age = newAge;
+    }
+    else{
+      console.error("Age must be a non-neg number");
+    }
+  }
+
+  get firstName(){
+    return this._firstName;
+  }
+  get lastName(){
+    return this._lastname;
+  }
+  get fullName(){
+    return this._firstName + " " + this._lastname;
+  }
+  get age(){
+    return this._age;
   }
 }
 
-class Rabbit extends Animal{
-  constructor(name, age, runSpeed){
-    super(name, age);
-    this.runSpeed = runSpeed;
+const person = new Person("aaa", "bbb", 30);
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.fullName);
+console.log(person.age);
+
+
+
+/* 
+class Rectangle{
+  constructor(width, heigit){
+    this.width = width;
+    this.heigit = heigit;
   }
-  run(){
-    console.log(`The ${this.name} can run`);
-    super.move(this.runSpeed);
+
+  set width(newWidth){
+    if(newWidth > 0){
+      this._width = newWidth; 
+    }
+    else{
+      console.error("Width must be a pos number");
+    }
   }
-}
-class Fish extends Animal{
-  constructor(name, age, swimSpeed){
-    super(name, age);
-    this.swimSpeed = swimSpeed;
+  set heigit(newHeight){
+    if(newHeight > 0){
+      this._height = newHeight;
+    }
+    else{
+      console.error("Height must be a pos number");
+    }
   }
-}
-class Hawk extends Animal{
-  constructor(name, age, flySpeed){
-    super(name, age);
-    this.flySpeed = flySpeed;
+  get width(){
+    return `${this._width.toFixed(1)} cm`;
+  }
+  get heigit(){
+    return `${this._height.toFixed(1)} cm`;
+  }
+  get area(){
+    return `${(this._width * this._height).toFixed(1)} cm^2`;
   }
 }
 
-const rabbit = new Rabbit("rabbit", 1, 25);
-const fish = new Fish("fish", 2, 12);
-const hawk = new Hawk("hawk", 3, 50);
+const rectangle = new Rectangle(3, 4);
 
-rabbit.run();
+rectangle.width = 3;
+rectangle.heigit = 6;
 
-console.log(rabbit.name);
-console.log(rabbit.age);
-console.log(rabbit.runSpeed);
+console.log(rectangle.width);
+console.log(rectangle.heigit);
+console.log(rectangle.area); 
+*/
