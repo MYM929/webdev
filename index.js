1c8cbfa19e612bbb5058d3a104599899adabdc29
mym <maiyongming929@gmail.com>
2024-05-26 23:16:34 -0500

    js #53: Digital Clock program

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
-
-let timeoutId;
-
-function startTimer(){
-    timeoutId = setTimeout(() => {window.alert("Hello");}, 3000);
-    console.log("STARTED");
+function updateClock(){
+    const now = new Date();
+    let hours = now.getHours();
+    const meridiem = hours>=12 ? "PM": "AM";
+    hours = hours%12 || 12; 
+    hours = hours.toString().padStart(2,0);
+    const minutes = now.getMinutes().toString().padStart(2,0);
+    const seconds = now.getSeconds().toString().padStart(2,0);
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.getElementById("clock").textContent = timeString;
 }
-function clearTimer(){
-    clearTimeout(timeoutId);
-    console.log("CLEARED");
-}
 
-
-
-/* 
-const timeoutId = setTimeout(() => {window.alert("Hello");}, 3000);
-clearTimeout(timeoutId); 
-*/
-
-/*
-setTimeout(() => {window.alert("Hello");}, 3000);
-*/
-
-/* 
-function sayHello(){
-    window.alert("Hello");
-} 
-
-setTimeout(sayHello, 3000);
-*/
+updateClock();
+setInterval(updateClock, 1000);
