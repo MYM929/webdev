ada32cb8112abc7fc31cd99a79585b62298af2d3
mym <maiyongming929@gmail.com>
2024-05-27 00:39:06 -0500

    js #57: Error handling

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-function func1(callback){
-    setTimeout(() => {
-        console.log("Task 1"); callback()
-    }, 3000);
+try{
+    const dividend = Number(window.prompt("Enter a dividend: "));
+    const divisor = Number(window.prompt("Enter a divisor: "));
+
+    if(divisor==0){
+        throw new Error("You can't divide by zero!");
+    }
+    if(isNaN(dividend) || isNaN(divisor)){
+        throw new Error("Values must be a number");
+    }
+
+    const result = dividend/divisor;
+    console.log(result);
+}
+catch(error){
+    console.error(error);
 }
 
-function func2(){
-    console.log("Task 2");
-    console.log("Task 3");
-    console.log("Task 4");
+console.log("You have reached the end!");   
+
+/* 
+try{
+    console.log(x);
+}
+catch(error){
+    console.error(error);
+}
+finally{
+    console.log("This always executes");
 }
 
-func1(func2);
+console.log("You have reached the end!");   
+*/
