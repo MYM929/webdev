6d58daabfaa0e009eb0f15bc0a91314aa7231ced
mym <maiyongming929@gmail.com>
2024-05-27 16:10:48 -0500

    js #59: What is the DOM?

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const display = document.getElementById("display");
+/* 
+DOM = Document Object Model
+    object{} that represents the page you see in the web browser
+    and provides you with an API to interact with it.
+    web browser constructs the DOM when it loads an HTML document,
+    and structures all the elements in a tree-like representation.
+    JS can access the DOM to dynamically
+    change the content, structure, and style of a web page.
+*/
 
-function appendToDisplay(input){
-    display.value += input;
-}
-function clearDisplay(){
-    display.value = "";
-}
-function calculate(){
-    try{
-        display.value = eval(display.value);
-    }
-    catch(error){
-        display.value = "Error";
-    }
-}
+document.getElementById(""); //document is the DOM object, it has a function of getDocumentById()
+console.log(document); //display the HTML document
+console.dir(document); //display its properties
+document.title = "Hello";
+document.body.style.backgroundColor = "hsl(0, 0%, 15%)";
+
+const username = "Bro Code";
+const welcomeMsg = document.getElementById("welcome-msg");
+welcomeMsg.textContent += username==="" ? ` Guest` : " " + username;
