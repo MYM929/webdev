5ec0845303f7cb150b8fe0056e2f97a414bd9031
mym <maiyongming929@gmail.com>
2024-05-27 22:44:06 -0500

    js #61: DOM navigation

--- index.js
+++ index.js
@@ -1,93 +1,85 @@
-// const fruits = document.querySelectorAll(".fruits");
 
-// fruits[0].style.backgroundColor = "yellow";
-// fruits[1].style.backgroundColor = "yellow";
+const element = document.getElementById("fruits");
+const children = element.children;
+console.log(children);
 
-const foods = document.querySelectorAll("li");
+// children[1].style.backgroundColor = "yellow";
 
-// foods[0].style.backgroundColor = "yellow";
-// foods[1].style.backgroundColor = "yellow";
-
-// console.log(foods);
-
-foods.forEach(food => {
-    food.style.backgroundColor = "yellow";
-});
+Array.from(children).forEach(child => {
+    child.style.backgroundColor = "yellow";
+})
 
 
 
 /* 
-const element1 = document.querySelector(".fruits");
-element1.style.backgroundColor = "yellow";
-
-const element2 = document.querySelector("h4");
-element2.style.backgroundColor = "yellow";
-
-const element3 = document.querySelector("ul");
-element3.style.backgroundColor = "yellow";
-
-const element4 = document.querySelector("ol");
-console.log(element4); //return null  
-*/
-
-
-/* 
-const h4Elements = document.getElementsByTagName("h4");
-const liElements = document.getElementsByTagName("li");
-
-console.log(h4Elements);
-
-// h4Elements[0].style.backgroundColor = "yellow";
-
-// for(let h4Element of h4Elements){
-//     h4Element.style.backgroundColor = "yellow";
-// }
-
-// for(let liElement of liElements){
-//     liElement.style.backgroundColor = "lightgreen";
-// }
-
-Array.from(h4Elements).forEach(h4Element => {
-    h4Element.style.backgroundColor = "yellow";
-});
-
-Array.from(liElements).forEach(liElement => {
-    liElement.style.backgroundColor = "lightgreen";
-}); 
-*/
-
-
-/* 
-const fruits = document.getElementsByClassName("fruits");
-console.log(fruits);
-
-// fruits[0].style.backgroundColor = "yellow";
-// fruits[1].style.backgroundColor = "blue";
-
-// for(let fruit of fruits){
-//     fruit.style.backgroundColor = "yellow";
-// }
-
-Array.from(fruits).forEach(fruit => {
-    fruit.style.backgroundColor = "yellow";
-}); 
+const element = document.getElementById("apple");
+const parent = element.parentElement;
+parent.style.backgroundColor = "yellow"; 
 */
 
 
 
 
 /* 
-const myHeading = document.getElementById("my-heading");
-myHeading.style.backgroundColor = "yellow";
-myHeading.style.textAlign = "center";
+// const element = document.getElementById("orange");
+// const prevSibling = element.previousElementSibling;
+// prevSibling.style.backgroundColor = "yellow";
 
-console.log(myHeading); 
+const element = document.getElementById("desserts");
+const prevSibling = element.previousElementSibling;
+prevSibling.style.backgroundColor = "yellow";  
 */
+
+
+
+/* 
+// const element = document.getElementById("apple");
+// const nextSibling = element.nextElementSibling;
+// nextSibling.style.backgroundColor = "yellow";
+
+const element = document.getElementById("fruits");
+const nextSibling = element.nextElementSibling;
+nextSibling.style.backgroundColor = "yellow"; 
+*/
+
+
+
+/* 
+// const element = document.getElementById("fruits");
+// const lastChild = element.lastElementChild;
+// lastChild.style.backgroundColor = "yellow";
+
+const ulElements = document.querySelectorAll("ul");
+
+ulElements.forEach(ulElement => {
+    const lastChild = ulElement.lastElementChild;
+    lastChild.style.backgroundColor = "yellow";
+})  
+*/
+
+
+
+
+/* 
+// const element = document.getElementById("fruits");
+// const firstChild = element.firstElementChild;
+// firstChild.style.backgroundColor = "yellow";
+
+const ulElements = document.querySelectorAll("ul");
+
+ulElements.forEach(ulElement => {
+    const firstChild = ulElement.firstElementChild;
+    firstChild.style.backgroundColor = "yellow";
+}) 
+*/
+
+
 
 /*
-1. document.getElementById()       //element or null
-2. document.getElementsClassName() //HTML collection
-3. document.getElementsByTagName   //HTML collection
-4. document.querySelector()        //element or null
-5. document.querySelectorAll()     //nodelist
-*/   
+.firstElementChild
+.lastElementChild
+.nextElementSibling
+.previousElementSibling
+.parentElement
+.children  
+*/
