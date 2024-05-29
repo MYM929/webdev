9f0e405fdddf520f6d89aea095c9314e8945a63c
mym <maiyongming929@gmail.com>
2024-05-28 23:39:39 -0500

    js #66: NodeLists

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-const myButton = document.getElementById("myButton");
-const myImg = document.getElementById("myImg");
+let buttons = document.querySelectorAll(".myButtons");
 
-myButton.addEventListener("click", event =>{
-    if(myImg.style.visibility === "hidden"){
-        myImg.style.visibility = "visible";
-        myButton.textContent = "Hide";
-    }
-    else{
-        myImg.style.visibility = "hidden";
-        myButton.textContent = "Show";
-    }
+buttons.forEach(button => {
+    button.addEventListener("click", event => {
+        event.target.remove();
+        console.log(buttons);
+        buttons = document.querySelectorAll(".myButtons");
+        console.log(buttons);
+    });
 });
 
-// myButton.addEventListener("click", event =>{
-//     if(myImg.style.display === "none"){
-//         myImg.style.display = "block";
-//         myButton.textContent = "Hide";
-//     }
-//     else{
-//         myImg.style.display = "none";
-//         myButton.textContent = "Show";
-//     }
-// });
+
+/* 
+const newButton = document.createElement("button");
+newButton.textContent = "Button 5";
+newButton.classList = "myButtons";
+document.body.appendChild(newButton);
+
+console.log(buttons);
+
+buttons = document.querySelectorAll(".myButtons");
+console.log(buttons); 
+*/
+
+
+/* 
+buttons.forEach(button => {
+    button.addEventListener("mouseover", event => {
+        event.target.style.backgroundColor = "hsl(205, 100%, 40%)";
+    });
+});
+buttons.forEach(button => {
+    button.addEventListener("mouseout", event => {
+        event.target.style.backgroundColor = "hsl(205, 100%, 60%)";
+    });
+}); 
+*/
+
+
+
+/* 
+buttons.forEach(button => {
+    button.addEventListener("click", event => {
+        event.target.style.backgroundColor = "tomato";
+    });
+}); 
+*/
+
+
+
+// console.log(buttons);
+
+/* 
+buttons.forEach(button => {
+    button.style.backgroundColor = "green";
+    button.textContent += "😘";
+}) 
+*/
