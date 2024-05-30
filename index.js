0b59073a44a8b9b773669c9444659961e41df3a1
mym <maiyongming929@gmail.com>
2024-05-30 18:23:50 -0500

    js #74: JSON files commit 2

--- index.js
+++ index.js
@@ -7,6 +7,43 @@ JSON = (JS Object Notation) data-interchange format
        JSON.parse() = converts a JSON string to a JS Object
 */
 
+fetch("names.json")
+    .then(response => response.json())
+    .then(value => console.log(value))
+    .catch(error => console.log(error))
+fetch("person.json")
+    .then(response => response.json())
+    .then(value => console.log(value))
+    .catch(error => console.log(error))
+fetch("people.json")
+    .then(response => response.json())
+    .then(value => console.log(value))
+    .catch(error => console.log(error))
+fetch("people.json")
+    .then(response => response.json())
+    .then(values => values.forEach(value => console.log(value.name)))
+    .catch(error => console.log(error))
+
+
+/* 
+const jsonNames  = `["Spongebob", "Patrick", "Squidward", "Sandy"]`;
+const jsonPerson = `{"name": "Spongebob", "age": 30, "isEmployed": true, "hobbies": ["Jellyfishing", "Karate", "Cooking"]}`;
+const jsonPeople = `[{"name": "Spongebob", "age": 20, "isEmployed": true}, 
+                    {"name": "Patrick", "age": 34, "isEmployed": false}, 
+                    {"name": "Squidward", "age": 50, "isEmployed": true},
+                    {"name": "Sandy", "age": 27, "isEmployed": false}]`;
+
+const parseData1 = JSON.parse(jsonNames);
+const parseData2 = JSON.parse(jsonPerson);
+const parseData3 = JSON.parse(jsonPeople);
+
+console.log(parseData1);
+console.log(parseData2);
+console.log(parseData3); 
+*/
+
+
+/* 
 const names = ["Spongebob", "Patrick", "Squidward", "Sandy"];
 const person = {
        "name": "Spongebob", 
@@ -14,12 +51,38 @@ const person = {
        "isEmployed": true,
        "hobbies": ["Jellyfishing", "Karate", "Cooking"]
 };
+const people = [{
+    "name": "Spongebob", 
+    "age": 20,
+    "isEmployed": true
+}, 
+{
+    "name": "Patrick", 
+    "age": 34,
+    "isEmployed": false
+}, 
+{
+    "name": "Squidward", 
+    "age": 50,
+    "isEmployed": true
+},
+{
+    "name": "Sandy", 
+    "age": 27,
+    "isEmployed": false
+}
+];
 
-const jsonString1 = JSON.stringify(names);
-const jsonString2 = JSON.stringify(person);
+const jsonString1 = JSON.stringify(names);  //array of strings
+const jsonString2 = JSON.stringify(person); //one object
+const jsonString3 = JSON.stringify(people); //array of objects
 
 console.log(names);
 console.log(jsonString1);
 
 console.log(person);
 console.log(jsonString2);
+
+console.log(people);
+console.log(jsonString3); 
+*/
