ee975ef43d074c8d6cf4242eeec13bb9e478c9cf
mym <maiyongming929@gmail.com>
2024-05-30 12:15:49 -0500

    js #74 JSON files commit 1

--- index.js
+++ index.js
@@ -1,71 +1,12 @@
-function walkDog(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            const dogWalked = true;
-            if(dogWalked){
-                resolve("You walk the dog");
-            }
-            else{
-                reject("You didn't walk the dog");
-            }
-        }, 1500);
-    });
-}
-function cleanKitchen(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            const kitchenCleaned = true;
-            if(kitchenCleaned){
-                resolve("You clean the kitchen");
-            }
-            else{
-                reject("You didn't clean the kitchen");
-            }
-        }, 2500);
-    });
-}
-function takeOutTrash(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            const trashTakenOut = false;
-            if(trashTakenOut){
-                resolve("You take out the trash");
-            }
-            else{
-                reject("You didn't take out the trash");
-            }
-        }, 500);
-    });
-}
-
-async function doChores(){
-    try{
-        const walkDogResult = await walkDog();
-        console.log(walkDogResult);
-        const cleanKitchenResult = await cleanKitchen();
-        console.log(cleanKitchenResult);
-        const takeOutTrashResult = await takeOutTrash();
-        console.log(takeOutTrashResult);
-        console.log("Finished");
-    }
-    catch(error){
-        console.error("You didn't walk the dog");
-    }
-}
-
-doChores();
-
 /*
-walkDog().then(value => {console.log(value); return cleanKitchen()})
-         .then(value => {console.log(value); return takeOutTrash()})
-         .then(value => {console.log(value); console.log("Finished");})
-         .catch(error => console.error(error));
+JSON = (JS Object Notation) data-interchange format
+       Used for exchanging data between a server and a web app
+       JSON files {key:value} OR [value1, value2, value3]
+
+       JSON.stringify() = converts a JS object to a JSON string
+       JSON.parse() = converts a JSON string to a JS Object
 */
 
-/* 
-walkDog(() => {
-    cleanKitchen(() => {
-        takeOutTrash(() => console.log("Finished"));
-    });
-}); 
-*/
+const names = ["Spongebob", "Patrick", "Squidward", "Sandy"];
+const jsonString = JSON.stringify(names);
+console.log(names);
