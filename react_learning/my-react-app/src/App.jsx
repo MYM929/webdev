128fe65bab50f0ddb7ff3190ae8fbc936eb1a14f
mym <maiyongming929@gmail.com>
2024-06-01 00:03:23 -0500

    react #4: props

--- react_learning/my-react-app/src/App.jsx
+++ react_learning/my-react-app/src/App.jsx
@@ -1,7 +1,15 @@
-import Button from './Button.jsx'
+import Student from './Student.jsx'
 
 function App() {
-    return(<Button/>);
+  return(
+    <>
+      <Student name="aaa" age={30} isStudent={true}/>
+      <Student name="bbb" age={42} isStudent={false}/>
+      <Student name="ccc" age={50} isStudent={false}/>
+      <Student name="ddd" age={27} isStudent={true}/>
+      <Student />
+    </>
+  );
 }
 
 export default App
