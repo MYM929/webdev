517134997f27efaaa463044aab448741d4869f87
mym <maiyongming929@gmail.com>
2024-06-01 23:21:42 -0500

    react #10: color picker app

--- react_learning/my-react-app/src/App.jsx
+++ react_learning/my-react-app/src/App.jsx
@@ -1,10 +1,10 @@
-import MyComponent from "./MyComponent.jsx";
+import ColorPicker from "./ColorPicker.jsx";
 
 function App() {
 
   return(
     <>
-      <MyComponent/>
+      <ColorPicker/>
     </>
   );
 }
