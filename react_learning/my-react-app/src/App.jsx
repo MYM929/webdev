963206be7358df56f5508319200b25195d947aee
mym <maiyongming929@gmail.com>
2024-06-02 18:06:39 -0500

    reac #11 updater functions

--- react_learning/my-react-app/src/App.jsx
+++ react_learning/my-react-app/src/App.jsx
@@ -1,4 +1,3 @@
-import MyComponent from "./MyComponent.jsx";
 import Counter from "./Counter.jsx";
 
 function App() {
--- react_learning/my-react-app/src/Counter.jsx
+++ react_learning/my-react-app/src/Counter.jsx
@@ -3,20 +3,31 @@ import React, {useState} from 'react';
 function Counter(){
     const [count, setCount] = useState(0);
     const increment = () => {
+        setCount(prevCount => prevCount+1);
+        setCount(prevCount => prevCount+1);
+        setCount(prevCount => prevCount+1);
+
+        /*
+        //this will not work
         setCount(count+1);
+        setCount(count+1);
+        setCount(count+1);
+        */
     }
     const decrement = () => {
-        setCount(count-1);
+        setCount(prevCount => prevCount-1);
+        setCount(prevCount => prevCount-1);
+        setCount(prevCount => prevCount-1);
     }
     const reset = () => {
         setCount(0);
     }
 
-    return(<div className="counter-container">
-        <p className="count-display">{count}</p>
-        <button className="counter-button" onClick={decrement}>Decrement</button>
-        <button className="counter-button" onClick={reset}>Reset</button>
-        <button className="counter-button" onClick={increment}>Increment</button>
+    return(<div>
+        <p>{count}</p>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={increment}>Increment</button>
     </div>);
 }
 
