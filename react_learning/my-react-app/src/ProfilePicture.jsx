c9a1226887c9d352e15ec756f0502b89e409c7d4
mym <maiyongming929@gmail.com>
2024-06-01 21:18:35 -0500

    react #8: useState() hook

--- react_learning/my-react-app/src/ProfilePicture.jsx
+++
@@ -1,9 +0,0 @@
-function ProfilePicture(){
-    const imageUrl = './src/assets/Logo-warga-200x200.png';
-    const handleClick = (e) => {
-        e.target.style.display = "none";
-    }
-    return(<img src={imageUrl} onClick={(e) => handleClick(e)}></img>);
-}
-
-export default ProfilePicture
