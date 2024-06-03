6c29efcb3651f22ef84cf42399117eb4b7889b85
mym <maiyongming929@gmail.com>
2024-06-03 08:59:56 -0500

    react #17: digital clock app commit 1

--- react_learning/my-react-app/src/App.jsx
+++ react_learning/my-react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import MyComponent from "./MyComponent.jsx";
+import DigitalClock from "./DigitalClock.jsx";
 
 function App() {
 
   return(
     <>
-      <MyComponent/>
+      {/* <MyComponent/> */}
+      <DigitalClock/>
     </>
     
   );
