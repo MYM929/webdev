175b68a9b8d16daf9014ef1c4c697d446ff7c008
mym <maiyongming929@gmail.com>
2024-06-02 19:21:39 -0500

    cherry-pick #12

--- react_learning/my-react-app/src/MyComponent.jsx
+++ react_learning/my-react-app/src/MyComponent.jsx
@@ -1,33 +1,32 @@
 import React, {useState} from "react";
 
 function MyComponent(){
-    const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
+    const [car, setCar] = useState({year: 2024, make: "Ford", model: "Mustang"});
 
-    function handleAddFood(){
-        const newFood = document.getElementById("foodInput").value;
-        document.getElementById("foodInput").value = "";
+    function handleYearChange(event){
+        // setCar({...car, year: event.target.value});
+        setCar(prevCar => (
+            {...prevCar, year: event.target.value}
+        ));
+    }
+    function handleMakeChange(event){
+        setCar(prevCar => (
+            {...prevCar, make: event.target.value}
+        ));
+    }
+    function handleModelChange(event){
+        setCar(prevCar => (
+            {...prevCar, model: event.target.value}
+        ));
+    }
 
-        setFoods(prevFoods => [...prevFoods, newFood]);
-    }
-    function handleRemoveFood(index){
-        /*creates a new array that includes all items 
-        from the foods array except the one at the 
-        specified index*/
-        setFoods(foods.filter((_, i) => i!==index));
-    }
     return(<div>
-        <h2>List of Food</h2>
-        <ul>
-            {foods.map((food, index) => (
-                <li key={index} onClick={() => handleRemoveFood(index)}>
-                    {food}
-                </li>
-            ))}
-        </ul>
-        <input type="text" id="foodInput" placeholder="Enter food name"/>
-        <button onClick={handleAddFood}>Add Food</button>
+        <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
+
+        <input type="number" value={car.year}  onChange={handleYearChange}/> <br />
+        <input type="text"   value={car.make}  onChange={handleMakeChange}/> <br />
+        <input type="text"   value={car.model} onChange={handleModelChange}/><br />
     </div>);
-    
 }
 
 export default MyComponent
