9669158f6c3ecf99cd19a7a27393b86c75ed00e7
mym <maiyongming929@gmail.com>
2024-06-05 09:02:04 -0500

    web #5: Navbar design commit 2

--- Portfolio/src/components/navbar/Navbar.jsx
+++ Portfolio/src/components/navbar/Navbar.jsx
@@ -4,7 +4,15 @@ const Navbar = () => {
   return(
     <div className="navbar">
       {/* Sidebar */}
-      .wrapper
+      <div className="wrapper">
+        <span>Lama Dev</span>
+        <div className="social">
+          <a href="#"><img src="/facebook.png" alt="" /></a>
+          <a href="#"><img src="/instagram.png" alt="" /></a>
+          <a href="#"><img src="/youtube.png" alt="" /></a>
+          <a href="#"><img src="/dribbble.png" alt="" /></a>
+        </div>
+      </div>
     </div>
   );
 }
