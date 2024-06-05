5e5a4d91c807d5de2fe7a4376d8a777750794e42
mym <maiyongming929@gmail.com>
2024-06-05 00:10:53 -0500

    web #5 Navbar design commit 1

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,8 +1,11 @@
 import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return <div>
-    <section><a href="">Hello</a></section>
+    <section>
+      <Navbar></Navbar>
+    </section>
     <section>Parallax</section>
     <section>Services</section>
     <section>Parallax</section>
