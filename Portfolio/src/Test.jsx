285cd53bb664ba695e01f4c4fe2fc0b2a9c6448f
mym <maiyongming929@gmail.com>
2024-06-07 12:18:25 -0500

    web #6: React Framer Motion Crash Course commit 5

--- Portfolio/src/Test.jsx
+++ Portfolio/src/Test.jsx
@@ -5,19 +5,23 @@ const Test = () => {
   const [open, setOpen] = useState(false);
 
   const variants = {
-    visible: {opacity: 1, x:800, transition:{type:"spring", stiffness:100, damping:100}},
-    hidden: {opacity: 0},
-  }
-
+    visible: (i)=>({
+        opacity: 1, 
+        x:100, 
+        transition:{delay: 1*0.3},
+      }),
+      hidden: {opacity: 0},
+    }
+    
   const items = [
     "item1", "item2", "item3", "item4" 
   ]
 
   return (
     <div className="course">
-      <motion.ul>
-        {items.map((item)=>(
-          <motion.li key={item}>{item}</motion.li>
+      <motion.ul initial="hidden" animate="visible" variants={variants}>
+        {items.map((item, i)=>(
+          <motion.li variants={variants} key={item} custom={i}>{item}</motion.li>
         ))}
       </motion.ul>
     </div>
