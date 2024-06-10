00dcc6264cc6777584f98357cf91582392ec0dd2
mym <maiyongming929@gmail.com>
2024-06-10 00:26:20 -0500

    PHP #4:  and

--- PHP_learning/index.php
+++ PHP_learning/index.php
@@ -1,15 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <label>quantity</label><br>
+        <input type="text" name="quantity">
+        <input type="submit" value="total">
+    </form>
+</body>
+
+</html>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+         <form action="index.php" method="get"> 
+        <label>username:</label><br>
+        <input type="text" name="username"><br>
+        <label>password:</label><br>
+        <input type="password" name="password"><br>
+        <input type="submit" value="log in">
+    </form>
+</body>
+
+</html> -->
+
+
 <?php
-$x = 10;
-$y = 2;
-$z = null;
-$counter = 0;
+$item = "pizza";
+$price = 5.99;
+$quantity = $_POST["quantity"];
+$total = null;
+$total = $quantity * $price;
 
-// $z = $x + $y;
-// $z = $x ** $y; //10^2
+echo "You have ordered {$quantity} x {$item}/s <br>";
+echo "Your total is: \${$total}";
+/*
+echo "{$_POST["username"]} <br>";
+echo "{$_POST["password"]} <br>";
 
-$counter++;
+// echo "{$_GET["username"]} <br>";
+// echo "{$_GET["password"]} <br>";
 
-// echo $z;
-// echo $counter;
-
-echo $total = 1 + 2 - 3 * 4 / 5 ** 6;
+// echo $_GET["username"] . "<br>";
+// echo $_GET["password"] . "<br>";
+*/
+?>
