98321491315b5460cbe7e02771b042aa2e4ed816
mym <maiyongming929@gmail.com>
2024-06-10 23:56:22 -0500

    PHP #9: for loops

--- PHP_learning/index.php
+++ PHP_learning/index.php
@@ -1,13 +1,23 @@
-<?php
-    $date = date("l"); //give the date of the week
-    // echo "{$date}";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>Enter a number to count to:</label>
+        <input type="text" name="counter">
+        <input type="submit" value="start">
+    </form>
+</body>
+</html>
 
-    switch($date){
-        case "Monday":
-            echo "Monday"; break;
-        case "Tuesday":
-            echo "Tuesday"; break;
-        default:
-          echo "{$date} is not a day";
+<?php
+    $counter = $_POST["counter"];
+
+    for($i=0; $i<=$counter; $i++){
+        echo $i . "<br>";
     }
 ?>
