fc7a5e6eb1b4f1cb7fc55cbdd1aa745c805db6cd
mym <maiyongming929@gmail.com>
2024-06-11 23:41:13 -0500

    PHP #12: associative arrays

--- PHP_learning/index.php
+++ PHP_learning/index.php
@@ -1,15 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>Enter a country</label>
+        <input type="text" name="country">
+        <input type="submit">
+    </form>
+</body>
+</html>
+
 <?php
-    $foods = array("apple", "orange", "banana", "coconut");
+    $capitals = array("USA"=>"Washington D.C.", 
+                      "Japan"=>"Kyoto", 
+                      "South Korea"=>"Seoul", 
+                      "India"=>"New Delhi");
 
-    // $foods[0] = "pineapple";
-    // array_push($foods, "pineapple", "kiwi");
-    // array_pop($foods);
-    // array_shift($foods);
-    // $foods = array_reverse($foods);
+    $capital = $capitals[$_POST["country"]];
+    echo "The capital is {$capital}";
 
-    foreach($foods as $food){
-        echo $food . "<br>";
-    }
+    // $capitals["USA"] = "Las Vegas";
+    // $capitals["China"] = "Beijing";
+    // array_pop($capitals);
+    // array_shift($capitals);
+    // $keys = array_keys($capitals);
+    // $values = array_values($capitals);
+    // $capitals = array_flip($capitals);
+    // $capitals = array_reverse($capitals);
 
-    echo count($foods);
+    // foreach($capitals as $key => $value){
+    //     echo "{$key} = {$value} <br>";
+    // }
+
+    // echo count($capitals);
+    // foreach($keys as $key){
+    //     echo "{$key} <br>";
+    // }
+    // foreach($values as $value){
+    //     echo "{$value} <br>";
+    // }
 ?>
