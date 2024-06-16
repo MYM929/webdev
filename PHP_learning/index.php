e24407112ce197fefa2078a7563e13a24f43d5eb
mym <maiyongming929@gmail.com>
2024-06-15 19:36:42 -0500

    PHP #17: string functions

--- PHP_learning/index.php
+++ PHP_learning/index.php
@@ -1,13 +1,32 @@
 <?php
-    function is_even(int $number){
-        return $number%2;
-    }
-    
-    echo is_even(11);
+    $username = "Bro the code";
+    $username1 = array("Bro", "the", "code");
+    $phone = "123-456-7890";
 
-    // function happy_birthday($first_name, $age){
-    //     echo "Happy Birthday {$first_name} {$age}";
+    // $username = strtolower($username);
+    // $username = strtoupper($username);
+    // $username = trim($username);
+    // $username = str_pad($username, 10, "0");
+    // $phone = str_replace("-", "", $phone);
+    // $username = strrev($username);
+    // $username = str_shuffle($username);
+    // $equals = strcmp($username, "Bro code");
+    // $count = strlen($username);
+    // $index = strpos($username, " ");
+    // $firstname = substr($username, 0, 3);
+    // $lastname = substr($username, 4);
+    // $fullname = explode(" ", $username);
+    $username1 = implode(" ", $username1);
+
+    echo $username . "<br>";
+    echo $phone . "<br>";
+    // echo $equals;
+    // echo $count;
+    // echo $index;
+    // echo $firstname;
+    // echo $lastname;
+    // foreach($fullname as $name){
+    //     echo $name . "<br>";
     // }
-
-    // happy_birthday("aaa", 30);
+    echo $username1;
 ?>
