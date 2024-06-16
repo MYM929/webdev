7968b90404be8aed972ebc527c8fe3e959b296f9
mym <maiyongming929@gmail.com>
2024-06-15 22:05:19 -0500

    PHP #21: SESSION

--- PHP_learning/index.php
+++ PHP_learning/index.php
@@ -1,18 +1,39 @@
 <?php
-    setcookie("fav_food", "pizza", time()+(86400*2), "/");
-    setcookie("fav_drink", "coffee", time()+(86400*3), "/");
-    setcookie("fav_dessert", "ice cream", time()+(86400*4), "/");
+    session_start();
+?>
 
-    // setcookie("fav_food", "pizza", time()-0, "/");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        username:<br>
+        <input type="text" name="username"><br>
+        password:<br>
+        <input type="password" name="password"><br>
+        <input type="submit" name="login" value="login">
+    </form>
+</body>
+</html>
 
-    foreach($_COOKIE as $key => $value){
-        echo "{$key} = {$value} <br>";
-    }
+<?php
+    if(isset($_POST["login"])){
+        if(!empty($_POST["username"]) && !empty($_POST["password"])){
+            $_SESSION["username"] = $_POST["username"];
+            $_SESSION["password"] = $_POST["password"];
+            
+            // echo $_SESSION["username"] . "<br>";
+            // echo $_SESSION["password"] . "<br>";
 
-    if(isset($_COOKIE["fav_food"])){
-        echo "BUY SOME {$_COOKIE["fav_food"]} !!!";
-    }
-    else{
-        echo "I don't know your fav food";
+            header("Location: home.php");
+        }
+        else{
+            echo "Missing username/password <br>";
+        }
     }
 ?>
+
