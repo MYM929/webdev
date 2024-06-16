042915efe1f06fc4bffa9140aa137553153d8ba3
mym <maiyongming929@gmail.com>
2024-06-16 10:51:46 -0500

    py #5: type cast

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,12 +1,11 @@
-name = "Bro"
+x = 1
+y = 2.0
+z = "3"
 
-# print(len(name))
-# print(name.find("B"))
-# print(name.capitalize())
-# print(name.upper())
-# print(name.lower)
-# print(name.isdight())
-# print(name.isalpha())
-# print(name.count("o"))
-# print(name.replace("o", "a"))
-# print(name*3)
+x = float(x)
+y = str(y)
+z = int(z)
+
+print(x)
+print(y)
+print(z*3)
