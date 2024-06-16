451f19dd1d74aff479bb5010e7f7a70ac4711f42
mym <maiyongming929@gmail.com>
2024-06-15 22:26:53 -0500

    PHP #23: password hashing

--- PHP_learning/index.php
+++ PHP_learning/index.php
@@ -1,25 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
-        username:<br>
-        <input type="text" name="username">
-        <input type="submit">
-    </form>
-</body>
-</html>
-
 <?php
-    foreach($_SERVER as $key => $value){
-        echo "{$key} = {$value} <br>";
-    }
+    $password = "pizza123";
+    $hash = password_hash($password, PASSWORD_DEFAULT);
 
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        echo "HELLO";
+    if(password_verify("pizza123", $hash)){
+        echo "You are logged in";
+    }
+    else{
+        echo "Incorrect password!";
     }
 ?>
