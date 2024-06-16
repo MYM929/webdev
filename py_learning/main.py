8aa715ed9eae7a90d94e32b2f93a0d1024187882
mym <maiyongming929@gmail.com>
2024-06-16 10:38:28 -0500

    py #2: variables

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,2 +1,25 @@
-print("I love pizza")
-print("It's really good")
+human = True
+print(human)
+print(type(human))
+
+
+
+# height = 250.5
+# print(height)
+# print(type(height))
+
+
+# age = 21
+# age += 1
+# print(age)
+# print(type(age))
+# print("Your age is: " + str(age))
+
+
+# first_name = "Bro"
+# last_name = "Code"
+# full_name = first_name + " " + last_name
+
+# print("Hello " + name)
+# print(type(name))
+# print(full_name)
