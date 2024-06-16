43c3054ce49d363f8cc97c5d2f7013cb0fb2a77b
mym <maiyongming929@gmail.com>
2024-06-15 19:13:11 -0500

    PHP #16: functions

--- PHP_learning/index.php
+++ PHP_learning/index.php
@@ -1,27 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <input type="checkbox"  name="foods[]" value="Pizza">Pizza<br>
-        <input type="checkbox"  name="foods[]" value="Hamburger">Hamburger<br>
-        <input type="checkbox"  name="foods[]" value="Hotdog">Hotdog<br>
-        <input type="checkbox"  name="foods[]" value="Taco">Taco<br>
-        <input type="submit"    name="submit">
-    </form>
-</body>
-</html>
-
 <?php
-    if(isset($_POST["submit"])){
-        $foods = $_POST["foods"];
-
-        foreach($foods as $food){
-            echo $food . "<br>";
-        }
+    function is_even(int $number){
+        return $number%2;
     }
+    
+    echo is_even(11);
+
+    // function happy_birthday($first_name, $age){
+    //     echo "Happy Birthday {$first_name} {$age}";
+    // }
+
+    // happy_birthday("aaa", 30);
 ?>
