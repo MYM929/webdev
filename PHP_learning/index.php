3267391499c103b1fe8d7126589304c498403067
mym <maiyongming929@gmail.com>
2024-06-15 21:04:59 -0500

    PHP #20: COOKIE

--- PHP_learning/index.php
+++ PHP_learning/index.php
@@ -1,20 +1,18 @@
 <?php
-    include("header.html");
-?>
+    setcookie("fav_food", "pizza", time()+(86400*2), "/");
+    setcookie("fav_drink", "coffee", time()+(86400*3), "/");
+    setcookie("fav_dessert", "ice cream", time()+(86400*4), "/");
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    This is the Home page<br>
-    Stuff about your home page can go here<br>
-</body>
-</html>
+    // setcookie("fav_food", "pizza", time()-0, "/");
 
-<?php
-    include("footer.html");
+    foreach($_COOKIE as $key => $value){
+        echo "{$key} = {$value} <br>";
+    }
+
+    if(isset($_COOKIE["fav_food"])){
+        echo "BUY SOME {$_COOKIE["fav_food"]} !!!";
+    }
+    else{
+        echo "I don't know your fav food";
+    }
 ?>
