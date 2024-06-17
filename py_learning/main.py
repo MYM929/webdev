b88a6055d088e293f835123ffe9528c6be358aa0
mym <maiyongming929@gmail.com>
2024-06-16 21:25:46 -0500

    py #15 lists

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,20 +1,15 @@
-for i in range(1,21):
-    if i == 13:
-        pass
-    else:
-        print(i)
+foods = ["pizza", "hamburger", "hotdog", "spaghetti"]
 
+foods[0] = "sushi"
 
+# foods.append("ice cream")
+# food.remove("hotdog")
+# foods.pop()
+# foods.insert(0, "cake")
+# foods.sort()
+# foods.clear()
 
-# phone_number = "123-456-7890"
-# for i in phone_number:
-#     if i == "-":
-#         continue
-#     print(i, end="") 
+print(foods[0])
 
-
-
-# while True:
-#     name = input("Enter your name: ")
-#     if name != "":
-#         break
+for x in foods:
+    print(x)
