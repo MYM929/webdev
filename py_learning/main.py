7c7e6ea54740181b9e7436cf8bc428c583faa11d
mym <maiyongming929@gmail.com>
2024-06-16 21:17:39 -0500

    py #13: nested loops

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,19 +1,8 @@
-import time
+rows = int(input("How many rows?: "))
+columns = int(input("How many columns?: "))
+symbol = input("Enter a symbol to use: ")
 
-for seconds in range(10, 0, -1):
-    print(seconds)
-    time.sleep(1)
-print("Happy New Year!")
-
-
-# for i in "Bro Code":
-#     print(iter)
-
-# for i in range(50,100+1,2):
-#     print(i)
-
-# for i in range(50,100+1):
-#     print(i)
-
-# for i in range(10):
-#     print(i)
+for i in range(rows):
+    for j in range(columns):
+        print(symbol, end="")
+    print()
