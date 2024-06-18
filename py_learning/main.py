f0cd100020c8d0a92c306c4764cd7eaea8b5c893
mym <maiyongming929@gmail.com>
2024-06-18 12:28:19 -0500

    py #28: string format

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,7 +1,26 @@
-def hello(**kwargs):
-    # print("Hello " + kwargs['first'] + " " + kwargs['last'])
-    print("Hello ", end="")
-    for key,value in kwargs.items():
-        print(value, end=" ")
+# animal = "cow"
+# item = "moon"
 
-hello(title="Mr.", first="Bro", middle="Dude", last="Code")
+# # print("The {} jumped over the {}".format(animal, item))
+# # print("The {1} jumped over the {0}".format(item, animal))
+# # print("The {animal} jumped over the {item}".format(animal="cow", item="moon"))
+# # print("The {animal} jumped over the {animal}".format(animal="cow", item="moon"))
+
+# text = "The {} jumped over the {}"
+# print(text.format(animal, item))
+
+# name = "Bro"
+# print("Hello, my name is {}".format(name))
+# print("Hello, my name is {:10}. Nice".format(name))
+# print("Hello, my name is {:<10}. Nice".format(name))
+# print("Hello, my name is {:>10}. Nice".format(name))
+# print("Hello, my name is {:^10}. Nice".format(name))
+
+# number = 3.14159
+# print("The number pi is {:.2f}".format(number))
+
+number = 1000
+print("The number pi is {:,}".format(number))
+print("The number pi is {:b}".format(number))
+print("The number pi is {:o}".format(number))
+print("The number pi is {:E}".format(number))
