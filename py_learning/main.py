67d560a659be7a2c83d3a64e2bcd2c9f38fa8c72
mym <maiyongming929@gmail.com>
2024-06-18 22:23:25 -0500

    py #35: move a file

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,5 +1,15 @@
-import shutil
+import os
 
-shutil.copyfile('text.txt', 'copy.txt')
-# shutil.copy('text.txt', 'copy.txt')
-# shutil.copy2('text.txt', 'copy.txt')
+source = 'text.txt'
+dest = 'C:\\Users\\mym\\Desktop\\move.txt'
+
+try:
+    if os.path.exists(dest):
+        print("There is already a file there")
+    else:
+        os.replace(source, dest)
+        print(source + " was moved")
+except FileNotFoundError:
+    print(source + " was not found")
+
+# can also move a dir
