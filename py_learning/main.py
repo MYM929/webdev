6bd38e43112cfee6be809b5129a6dabe933c2d64
mym <maiyongming929@gmail.com>
2024-06-18 21:51:24 -0500

    py #29: random numbers

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,26 +1,15 @@
-# animal = "cow"
-# item = "moon"
+import random
 
-# # print("The {} jumped over the {}".format(animal, item))
-# # print("The {1} jumped over the {0}".format(item, animal))
-# # print("The {animal} jumped over the {item}".format(animal="cow", item="moon"))
-# # print("The {animal} jumped over the {animal}".format(animal="cow", item="moon"))
+x = random.randint(1,6)
+y = random.random()
 
-# text = "The {} jumped over the {}"
-# print(text.format(animal, item))
+myList = ['rock', 'paper', 'scissors']
+z = random.choice(myList)
 
-# name = "Bro"
-# print("Hello, my name is {}".format(name))
-# print("Hello, my name is {:10}. Nice".format(name))
-# print("Hello, my name is {:<10}. Nice".format(name))
-# print("Hello, my name is {:>10}. Nice".format(name))
-# print("Hello, my name is {:^10}. Nice".format(name))
+cards = [1,2,3,4,5,6,7,8,9,"J","Q","K","A"]
+random.shuffle(cards)
 
-# number = 3.14159
-# print("The number pi is {:.2f}".format(number))
-
-number = 1000
-print("The number pi is {:,}".format(number))
-print("The number pi is {:b}".format(number))
-print("The number pi is {:o}".format(number))
-print("The number pi is {:E}".format(number))
+print(x)
+print(y)
+print(z)
+print(cards)
