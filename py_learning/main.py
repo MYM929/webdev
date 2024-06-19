0c0ac5e3f71b4059a751efbbd7c230b9e0107d26
mym <maiyongming929@gmail.com>
2024-06-18 22:14:03 -0500

    py #32: read a file

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,13 +1,7 @@
-import os
+try:
+    with open('text.txt') as file:
+        print(file.read())
+except FileNotFoundError:
+    print("That file was not found")
 
-path = "C:\\Users\\mym\\Desktop\\Mavs final\\New Text Document.txt"
-
-if os.path.exists(path):
-    print("That location exists!")
-    if os.path.isfile(path):
-        print("That is a file")
-    if os.path.isdir(path):
-        print("That is a dir")
-else:
-    print("That location doesn't exist!")
-
+print(file.closed)
