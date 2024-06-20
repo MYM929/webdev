bc799159423ef52f115e58dedc070f68bf6aee76
mym <maiyongming929@gmail.com>
2024-06-20 12:22:20 -0500

    py #40: Object Oriented Programming (OOP)

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,37 +1,8 @@
-import random
+from car import Car
 
-while True:
-    choices = ["rock","paper","scissors"]
+car1 = Car("aaa","bbb",2021,"ccc")
+car2 = Car("ddd","eee",2022,"fff")
 
-    computer = random.choice(choices)
-    player = None
-
-    while player not in choices:
-        player = input("rock, paper, or scissors?: ").lower()
-
-    print("Computer: ", computer)
-    print("Player: ", player)
-
-    if player == computer:
-        print("Tie")
-    elif player == "rock":
-        if computer == "paper":
-            print("You lose")
-        if computer == "scissors":
-            print("You win")
-    elif player == "scissors":
-        if computer == "rock":
-            print("You lose")
-        if computer == "paper":
-            print("You win")
-    elif player == "paper":
-        if computer == "scissors":
-            print("You lose")
-        if computer == "rock":
-            print("You win")
-    
-    play_again = input("Play again? (yes/no)").lower()
-    if play_again != "yes":
-        break #leave the loop
-
-print("Bye")
+print(car1.make)
+car1.drive()
+car1.stop()
