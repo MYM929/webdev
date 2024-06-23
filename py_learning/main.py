b8eea1db30aa39242b4a23515c8a1630a12c282d
mym <maiyongming929@gmail.com>
2024-06-23 18:59:27 -0500

    py #51: walrus operator

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,24 +1,16 @@
-class Duck:
-    def walk(self):
-        print("This duck is walking")
-    def talk(self):
-        print("This duck is qwuacking")
+foods = list()
+while food := input("What food do you like?: ") != "quit":
+    foods.appends(food)
 
-class Children:
-    def walk(self):
-        print("This chicken is walking")
-    def talk(self):
-        print("This children is clucking")
+# foods = list()
+# while True:
+#     food = input("What food do you like?: ")
+#     if food == "quit":
+#         break
+#     foods.append(food)
 
-class Person():
-    def catch(self, duck):
-        duck.walk()
-        duck.talk()
-        print("You caught the critter")
 
-duck = Duck()
-children = Children()
-person = Person()
+# print(happy := True)
 
-person.catch(duck)
-person.catch(children)
+# happy = True
+# print(happy)
