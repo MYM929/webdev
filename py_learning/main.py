ddf9821829b8012e24040a168971bbf90e7756bf
mym <maiyongming929@gmail.com>
2024-06-22 20:43:29 -0500

    py #43: multilevel inheritance

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,26 +1,15 @@
-class Animal:
+class Organism:
     alive = True
 
+class Animal(Organism):
     def eat(self):
         print("This animal is eating")
-    def sleep(self):
-        print("This animal is sleeping")
 
-class Rabbit(Animal):
-    def run(self):
-        print("This rabbit is running")
-class Fish(Animal):
-    def swim(self):
-        print("This fish is swimming")
-class Hawk(Animal):
-    def fly(self):
-        print("This hawk is flying")
+class Dog(Animal):
+    def bark(self):
+        print("This dog is barking")
 
-rabbit = Rabbit()
-fish = Fish()
-hawk = Hawk()
-
-print(rabbit.alive)
-fish.eat()
-hawk.sleep()
-rabbit.run()
+dog = Dog()
+print(dog.alive)
+dog.eat()
+dog.bark()
