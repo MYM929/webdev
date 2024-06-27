f589cf1370a2d9e4721e7f93b809aaf71e696cb1
mym <maiyongming929@gmail.com>
2024-06-27 12:36:02 -0500

    py #59: list comprehensions

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,11 +1,21 @@
-import functools
+students = [100,90,80,70,60,50,40,30,20,10]
+# passed_students = [i for i in students if i>=60]
+passed_students = [i if i>=60 else "Fail" for i in students]
+print(passed_students)
 
-factorial = [5,4,3,2,1]
-result = functools.reduce(lambda x,y:x*y, factorial)
-print(result)
+
+# students = [100,90,80,70,60,50,40,30,20,10]
+# passed_students = list(filter(lambda x:x>=60, students))
+# print(passed_students)
 
 
-# letters = ["H","E","L","L","O"]
-# word = functools.reduce(lambda x,y,:x+y , letters)
-# print(word)
 
+# squares = [i*i for i in range(1,11)]
+# print(squares)
+
+
+
+# squares = []
+# for i in range(1, 11):
+#     squares.append(i*i)
+# print(squares)
