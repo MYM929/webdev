90f9e992b5d065e4d77f2ac6a81cf8b867a1bf09
mym <maiyongming929@gmail.com>
2024-07-02 12:35:54 -0500

    py #63: time module

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,15 +1,20 @@
-# import module
+import time
+time_tuple = (2020, 4, 20, 4, 20, 0, 0, 0, 0)
+time_str = time.asctime(time_tuple)
+# time_str = time.mktime(time_tuple)
+print(time_str)
 
-# print(__name__)
-# print(module.__name__)
+# time_str = "20 April, 2020"
+# time_obj = time.strptime(time_str, "%d %B, %Y")
+# print(time_obj)
 
-# if __name__ == '__main__':
-#     print("Running this modile directly")
-# else:
-#     print("Running other modile indirectly")
+# # print(time.ctime(10000000))
+# # print(time.time())
+# # print(time.ctime(time.time()))
+# time_obj = time.localtime()
+# time_obj1 = time.gmtime()
 
-def hello():
-    print("Hello")
+# # print(time_obj)
 
-if __name__ == '__main__':
-    hello()
+# local_time = time.strftime("%B %d %Y %H:%M:%S", time_obj)
+# print(local_time)
