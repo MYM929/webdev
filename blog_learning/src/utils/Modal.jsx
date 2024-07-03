9546f34d312c47f806103dd35e8a6ba596b5b58f
mym <maiyongming929@gmail.com>
2024-07-03 00:16:29 -0500

    fixed modal bug

--- blog_learning/src/utils/Modal.jsx
+++ blog_learning/src/utils/Modal.jsx
@@ -6,9 +6,9 @@ const Modal = ({children, modal, setModal, hidden}) => {
         <div
           onClick={() => setModal(false)}
           className={`bg-white/50 fixed inset-0 z-10
-                      ${hidden} transition-all duration-500`}>
-          {children}
-        </div>
+                      ${hidden} transition-all duration-500`}
+        />
+        {children}
     </>
   )
 }
