a44dadf194c0db0571168b1e222705e329f671cd
mym <maiyongming929@gmail.com>
2024-07-10 12:34:31 -0500

    py #69: buttons

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,26 +1,28 @@
+
 from tkinter import *
 
 window = Tk()
 
-# photo = PhotoImage(file='...........')
+# photo = PhotoImage(file='...')
 
-label = Label(
-    window, 
-    text="Hello World", 
-    font=('Arial', 40, 'bold'),
-    fg='green',
-    bg='black',
-    relief=RAISED,
-    # relief=SUNKEN,
-    bd=10,
-    padx=20,
-    pady=20,
-    # image=photo,
-    # compound='bottom'
-)
-label.pack()
-# label.place(x=0, y=0)
+count = 0
 
+def click():
+    global count
+    count += 1
+    print(count)
+
+button = Button(window, text="click me", command=click, 
+                font=("Comic Sans", 30), fg="#00FF00", bg="black",
+                activeforeground="#00FF00", activebackground="black",
+                state=ACTIVE)
+
+# state=DISABLED
+# image=photo
+# compound='bottom'
 
 
+
+
+button.pack()
 window.mainloop()
