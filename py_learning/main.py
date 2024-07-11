a35e96c8a0227b260d94278d148ed908cbba498c
mym <maiyongming929@gmail.com>
2024-07-11 12:29:57 -0500

    py #71: checkbox

--- py_learning/main.py
+++ py_learning/main.py
@@ -2,30 +2,30 @@ from tkinter import *
 
 window = Tk()
 
-##########################################################
-def submit():
-    username = entry.get()
-    print(username)
-    entry.config(state=DISABLED)
+#######################################
+x = IntVar()
 
-def delete():
-    entry.delete(0, END)
+# py_photo = PhotoImage(file="...")
 
-def backspace():
-    entry.delete(len(entry.get())-1, END)
+def display():
+    if(x.get()==1):
+        print("Agree")
+    else:
+        print("Not Agree")
 
-entry = Entry(window, font=("Arial", 50), fg="#00FF00", bg="black",
-              show="*")
-entry.insert(0, 'sssssss')
+#######################################
+check_button = Checkbutton(window, text="I agree to you", 
+                           variable=x, onvalue=1, offvalue=0,
+                           command=display,
+                           font=("Arial", 20),
+                           fg="#00FF00", bg="black",
+                           activeforeground="#00FF00", activebackground="black",
+                           padx=25, pady=10)
 
-submit_button = Button(window, text="submit", command=submit)
-delete_button = Button(window, text="delete", command=delete)
-backspace_button = Button(window, text="backspace", command=backspace)
+# image=py_photo
+# compound='left'
 
-##########################################################
-entry.pack(side=LEFT)
-submit_button.pack(side=RIGHT)
-delete_button.pack(side=RIGHT)
-backspace_button.pack(side=RIGHT)
+
+#######################################
+check_button.pack()
 window.mainloop()
-
