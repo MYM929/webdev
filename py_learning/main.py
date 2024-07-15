9c1f840693c5c7d987be1b11abf7212558b9e8e6
mym <maiyongming929@gmail.com>
2024-07-15 12:44:45 -0500

    py #74: listbox

--- py_learning/main.py
+++ py_learning/main.py
@@ -4,30 +4,50 @@ window = Tk()
 
 #######################################
 def submit():
-    print(str(scale.get()))
+    food = []
+
+    for index in listbox.curselection():
+        food.insert(index, listbox.get(index))
+    print("You have ordered: ")
+    # print(listbox.get(listbox.curselection()))
+    for index in food:
+        print(index)
+
+def add():
+    listbox.insert(listbox.size(), entryBox.get())
+    listbox.config(height=listbox.size())
+
+def delete():
+    for index in reversed(listbox.curselection()):
+        listbox.delete(index)
+    # listbox.delete(listbox.curselection())
+    listbox.config(height=listbox.size())
 
 
 #######################################
-# hotImage = PhotoImage(file="...")
-# hotLabel = Label(image=hotImage)
-# coldImage = PhotoImage(file="...")
-# coldLabel = Label(image=coldImage)
+listbox = Listbox(window, 
+                  bg="#f7ffde",
+                  font=("Constantia", 35),
+                  width=12,
+                  selectmode=MULTIPLE)
+entryBox = Entry(window)
+submitButton = Button(window, text="submit", command=submit)
+addButton = Button(window, text="add", command=add)
+deleteButton = Button(window, text="delete", command=delete)
 
-scale = Scale(window, from_=0, to=100,
-              length=600,
-              orient=VERTICAL,
-              font=('Consolas', 20),
-              tickinterval=10,
-              showvalue=0,
-              troughcolor="#69EAFF",
-              fg="#FF1C00", bg="black")
-scale.set( 
-    (scale['from']-scale['to'])/2 + scale['to']
-)
-button = Button(window, text="submit", command=submit)
+
+listbox.insert(1, "pizza")
+listbox.insert(2, "pasta")
+listbox.insert(3, "garlic bread")
+listbox.insert(4, "soup")
+listbox.insert(5, "salad")
+
+listbox.config(height=listbox.size())
+
 #######################################
-# hotLabel.pack()
-scale.pack()
-button.pack()
-# coldLabel.pack()
+listbox.pack()
+entryBox.pack()
+submitButton.pack()
+addButton.pack()
+deleteButton.pack()
 window.mainloop()
