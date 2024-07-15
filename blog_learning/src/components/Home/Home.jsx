c402104e7d777ce95757e93c8bc421cc230a7797
mym <maiyongming929@gmail.com>
2024-07-14 21:01:49 -0500

    blog #22: finished Home.jsx; time stamp: 4:30:35

--- blog_learning/src/components/Home/Home.jsx
+++ blog_learning/src/components/Home/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Posts from '../Common/Posts/Posts'
+import Follow from './UserToFollow/Follow'
 
 const home = () => {
   return (
-    <div>home</div>
+    <section className='size flex gap-[5rem] relative'>
+
+
+
+      {/* Left side */}
+      <div className='flex-[2] py-10 mb-[4rem]'>
+        <Posts/> {/* Posts component */}
+      </div>
+
+      {/* Right side */}
+      <div className='hidden md:inline-block md:w-[21rem] p-7 border-l border-gray-300'>
+        <h3>Who to follow?</h3> {/* Some text */}
+        <Follow/>
+      </div>
+
+
+    </section>
   )
 }
 
