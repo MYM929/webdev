5037dd21fc8f03f8ad20b6e59a2cd621eb7687be
mym <maiyongming929@gmail.com>
2024-07-15 22:28:05 -0500

    blog #25: add backend for save and unsave post icon; time stamp: 4:57:17

--- blog_learning/src/components/Common/Posts/Actions/SavedPost.jsx
+++ blog_learning/src/components/Common/Posts/Actions/SavedPost.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CiSaveDown2 } from "react-icons/ci";
+import { Blog } from '../../../../Context/Context';
+import { deleteDoc, doc, setDoc } from 'firebase/firestore';
+import { db } from '../../../../firebase/firebase';
+import { toast } from "react-toastify";
+import useSingleFetch from '../../../hooks/useSingleFetch';
 
 const SavedPost = ({post}) => {
+
+  // stores isSaved state
+  const [isSaved, setIsSaved] = useState(false);
+  const { currentUser } = Blog();
+
+  // fetch subcollection data from Firebase
+  const { data, loading } = useSingleFetch(
+      "users", currentUser?.uid, "savePost"
+  );
+  // console.log(data);
+
+  // find if the user have saved this post or not
+  useEffect(() => {
+    setIsSaved(data && data.find((item) => item.id===post.id));
+  }, [data, post?.id])
+  
+
+
+  const handleSave = async () => {
+    try {
+        if(currentUser){
+            const saveRef = doc(db, "users", currentUser?.uid, 
+                "savePost", post?.id
+            );
+        
+            if(isSaved){
+                await deleteDoc(saveRef);
+                toast.success("Post has been unsaved");
+            }
+            else{
+                await setDoc(saveRef, {...post,});
+                toast.success("Post has been saved");
+            }
+        }   
+    } 
+    catch (error) {
+        
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <>
-        <button className='hover:opacity-60'>
-            <CiSaveDown2 className='text-2xl pointer-events-none'/>
+        <button onClick={handleSave} className='hover:opacity-60'> {/* Download/Save icon */}
+            <CiSaveDown2 className={`text-2xl pointer-events-none 
+                                    ${isSaved ? "text-yellow-600" : ""}`}/>
         </button>
     </>
   )
