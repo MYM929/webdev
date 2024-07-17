c779ea09f6e4bd05d7bde71e4a1141175d1c6b93
mym <maiyongming929@gmail.com>
2024-07-17 12:28:39 -0500

    py #76: colorchooser

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,52 +1,22 @@
 from tkinter import *
-from tkinter import messagebox
+from tkinter import colorchooser
 
 window = Tk()
 
 #######################################
 def click():
-    # messagebox.showinfo(title="This is an info message box",
-    #                     message="You are a person")
-    # while(True):
-    #     messagebox.showwarning(title="WARNING!", 
-    #                            message="You have a virus")
-    # messagebox.showerror(title="ERROR", 
-    #                      message="Something went wrong")
-    # if messagebox.askokcancel(title="ask ok cancel",
-    #                           message="Do you want to do the thing?"):
-    #     print("You did a thing")
-    # else:
-    #     print("You cancel a thing")
-    # if messagebox.askretrycancel(title="ask ok cancel",
-    #                              message="Do you want to retry the thing?"):
-    #     print("You retry a thing")
-    # else:
-    #     print("You cancel a thing")
-    # if messagebox.askyesno(title="ask yes or no",
-    #                        message="Do you like cake?"):
-    #     print("I like cake")
-    # else:
-    #     print("why not")
-    # answer = messagebox.askquestion(title="ask question",
-    #                                 message="do you like pie?")
-    # if(answer=="yes"):
-    #     print("like")
-    # else:
-    #     print("not like")
-    answer = messagebox.askyesnocancel(title="", message="Do you like to code?", icon=Warning)
-    if(answer == True):
-        print("like")
-    elif(answer == False):
-        print("do not")
-    else:
-        print("hello")    
+    # color = colorchooser.askcolor()
+    # # print(color)
+    # colorHex = color[1]
+    # # print(colorHex)
+    # window.config(bg=colorHex)
 
+    window.config(bg=colorchooser.askcolor()[1])
 
 
 #######################################
-button = Button(window, command=click, text="click me")
-
-
+window.geometry("420x420")
+button = Button(text='click me', command=click)
 
 #######################################
 button.pack()
