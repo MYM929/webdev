6b875622965197fbb4331858b0e2a7b2517b1f8a
mym <maiyongming929@gmail.com>
2024-07-18 12:42:00 -0500

    py #80: menubar

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,28 +1,44 @@
 from tkinter import *
-from tkinter import filedialog
+
 
 window = Tk()
 
 #######################################
+def openFile():
+    print("File has been open")
 def saveFile():
-    file = filedialog.asksaveasfile(initialdir="C:\\Users",
-                                    defaultextension='.txt',
-                                    filetypes=[
-                                        ("Text file", ".txt"),
-                                        ("HTML file", ".html"),
-                                        ("All files", ".*"),
-                                    ])
-    if file is None:
-        return
-    # fileText = str(text.get(1.0, END))
-    fileText = input("Enter some text: ")
-    file.write(fileText)
-    file.close()
+    print("File has been save")
+
+def cut():
+    print("Cut")
+def copy():
+    print("Copy")
+def paste():
+    print("Paste")
 #######################################
-button = Button(window, text="Save", command=saveFile)
-text = Text(window)
+menubar = Menu(window)
+window.config(menu=menubar)
+
+fileMenu = Menu(menubar, tearoff=0, font=("MV Boli", 15))
+menubar.add_cascade(label="File", menu=fileMenu)
+fileMenu.add_command(label="Open", command=openFile)
+fileMenu.add_command(label="Save", command=saveFile)
+fileMenu.add_separator()
+fileMenu.add_command(label="Exit",command=quit)
+
+editMenu = Menu(menubar, tearoff=0, font=("MV Boli", 15))
+menubar.add_cascade(label="Edit", menu=editMenu)
+editMenu.add_command(label="Cut", command=cut)
+editMenu.add_command(label="Copy", command=copy)
+editMenu.add_command(label="Paste", command=paste)
+
+# openImage = PhotoImage(file="...")
+# saveImage = PhotoImage(file="...")
+# exitImage = PhotoImage(file="...")
+# image=openImage, compound='left'
+# image=saveImage, compound='left'
+# image=exitImage, compound='left'
 
 #######################################
-button.pack()
-text.pack()
+
 window.mainloop()
