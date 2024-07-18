103ad469939c830b98e2efeb8867728ae5c90e90
mym <maiyongming929@gmail.com>
2024-07-18 17:16:56 -0500

    py #83: window tabs

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,14 +1,21 @@
 from tkinter import *
+from tkinter import ttk
 
 window = Tk()
 
 #######################################
-def create_window():
-    # new_window = Toplevel()
-    new_window = Tk()
-    window.destroy()
-#######################################
-Button(window, text="create new window", command=create_window).pack()
 
 #######################################
+notebook = ttk.Notebook(window)
+
+tab1 = Frame(notebook)
+tab2 = Frame(notebook)
+
+notebook.add(tab1, text="Tab 1")
+notebook.add(tab2, text="Tab 2")
+
+Label(tab1, text="this is tab 1", width=50, height=25).pack()
+Label(tab2, text="this is tab 2", width=50, height=25).pack()
+#######################################
+notebook.pack(expand=True, fill="both")
 window.mainloop()
