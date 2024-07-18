64fd6531eabf13825308b231b313d5eacbb942d7
mym <maiyongming929@gmail.com>
2024-07-18 12:26:22 -0500

    py #79: save a file(file dialog)

--- py_learning/main.py
+++ py_learning/main.py
@@ -4,19 +4,25 @@ from tkinter import filedialog
 window = Tk()
 
 #######################################
-def openFile():
-    filePath = filedialog.askopenfilename(initialdir="C:\\Users\\", 
-                                          title="Open File",
-                                          filetypes=(("text files", "*.txt"), 
-                                                     ("all files", "*.*")))
-    # print(filePath)
-    file = open(filePath, "r")
-    print(file.read())
+def saveFile():
+    file = filedialog.asksaveasfile(initialdir="C:\\Users",
+                                    defaultextension='.txt',
+                                    filetypes=[
+                                        ("Text file", ".txt"),
+                                        ("HTML file", ".html"),
+                                        ("All files", ".*"),
+                                    ])
+    if file is None:
+        return
+    # fileText = str(text.get(1.0, END))
+    fileText = input("Enter some text: ")
+    file.write(fileText)
     file.close()
-
 #######################################
-button = Button(window, text="Open", command=openFile)
+button = Button(window, text="Save", command=saveFile)
+text = Text(window)
 
 #######################################
 button.pack()
+text.pack()
 window.mainloop()
