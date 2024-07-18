8f71a35c7a5225def07e64a4857449b0d6d8e585
mym <maiyongming929@gmail.com>
2024-07-17 22:20:06 -0500

    blog #36: added backend to get post data for SinglePost.jsx; time stamp: 6:02:34

--- blog_learning/src/components/Common/Posts/SinglePost.jsx
+++ blog_learning/src/components/Common/Posts/SinglePost.jsx
@@ -1,6 +1,57 @@
-import React from 'react'
+import { doc, getDoc } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from "react-router-dom";
+import { db } from '../../../firebase/firebase';
+import { toast } from "react-toastify";
 
 const SinglePost = () => {
+
+  const { postId } = useParams();
+  const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+        setLoading(true);
+        try {
+            const postRef = doc(db, "posts", postId); 
+            const getPost = await getDoc(postRef);
+            
+            if(getPost.exists()){
+                const postData = getPost.data();
+                if(postData?.userId){
+                    const userRef = doc(db, "users", postData?.userId);
+                    const getUser = await getDoc(userRef);
+
+                    if(getUser.exists()){
+                        const userData = getUser.data();
+                        setPost({...postData, ...userData, id: postId});
+                    }
+                }
+            }
+            setLoading(false);
+        } 
+        catch (error) {
+            toast.error(error.message);
+            setLoading(false);
+        }
+    }
+    fetchPost();
+  }, [postId])
+  console.log(post);
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div>SinglePost</div>
   )
