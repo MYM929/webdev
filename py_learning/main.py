76bd292aa5ffc321663c9401981bb1b1a8203d09
mym <maiyongming929@gmail.com>
2024-07-18 16:56:04 -0500

    py #81: frames

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,44 +1,18 @@
 from tkinter import *
 
-
 window = Tk()
 
 #######################################
-def openFile():
-    print("File has been open")
-def saveFile():
-    print("File has been save")
-
-def cut():
-    print("Cut")
-def copy():
-    print("Copy")
-def paste():
-    print("Paste")
-#######################################
-menubar = Menu(window)
-window.config(menu=menubar)
-
-fileMenu = Menu(menubar, tearoff=0, font=("MV Boli", 15))
-menubar.add_cascade(label="File", menu=fileMenu)
-fileMenu.add_command(label="Open", command=openFile)
-fileMenu.add_command(label="Save", command=saveFile)
-fileMenu.add_separator()
-fileMenu.add_command(label="Exit",command=quit)
-
-editMenu = Menu(menubar, tearoff=0, font=("MV Boli", 15))
-menubar.add_cascade(label="Edit", menu=editMenu)
-editMenu.add_command(label="Cut", command=cut)
-editMenu.add_command(label="Copy", command=copy)
-editMenu.add_command(label="Paste", command=paste)
-
-# openImage = PhotoImage(file="...")
-# saveImage = PhotoImage(file="...")
-# exitImage = PhotoImage(file="...")
-# image=openImage, compound='left'
-# image=saveImage, compound='left'
-# image=exitImage, compound='left'
 
 #######################################
+frame = Frame(window, bg="pink", bd=5, relief=SUNKEN)
+frame.place(x=0, y=0)
 
+Button(frame, text="W", font=("Consolas", 25), width=3).pack(side=TOP)
+Button(frame, text="A", font=("Consolas", 25), width=3).pack(side=LEFT)
+Button(frame, text="S", font=("Consolas", 25), width=3).pack(side=LEFT)
+Button(frame, text="D", font=("Consolas", 25), width=3).pack(side=LEFT)
+
+#######################################
+# frame.pack()
 window.mainloop()
