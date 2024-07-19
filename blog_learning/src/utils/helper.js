43abc23b762b3bca645731ded88095b65751d522
mym <maiyongming929@gmail.com>
2024-07-18 22:42:06 -0500

    blog #38: added backend for like button in SinglePost.jsx; time stamp: 7:00:27

--- blog_learning/src/utils/helper.js
+++ blog_learning/src/utils/helper.js
@@ -19,3 +19,18 @@ export const readTime = (desc) => {
     const words = textContent.trim().split(/\s+/);
     return Math.ceil(words.length / avgReading);
 }
+
+
+export const formatNum = (num) => {
+    if (num >= 1e9)
+      return (num / 1e9).toFixed(1) + "B";
+
+    else if (num >= 1e6) 
+      return (num / 1e6).toFixed(1) + "M";
+
+    else if (num >= 1e3)
+      return (num / 1e3).toFixed(1) + "K";
+    
+    else
+      return num.toString();
+};
