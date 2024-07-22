fc406895c02c39c08f86a7417227dde1ce57a948
mym <maiyongming929@gmail.com>
2024-07-22 12:40:32 -0500

    py #86: canvas

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,36 +1,26 @@
 from tkinter import *
-from tkinter.ttk import *
-import time
+
 
 window = Tk()
 
 #######################################
-percent = StringVar()
-text = StringVar()
 
 
-def start():
-    GB = 100
-    download = 0
-    speed = 1
-    while(download<GB):
-        time.sleep(0.5)
-        bar['value']+=(speed/GB)*100
-        download+=speed
-        percent.set(str(int((download/GB)*100))+"%")
-        text.set(str(download)+"/"+str(GB)+" GB completed")
-        window.update_idletasks()
 #######################################
+canvas = Canvas(window, height=500, width=500)
 
-bar = Progressbar(window, orient=HORIZONTAL, length=300)
-bar.pack(pady=10)
+# canvas.create_line(0,0,500,500, fill="blue", width=5)
+# canvas.create_line(0,500,500,0, fill="red", width=5)
+# canvas.create_rectangle(50,50,250,250, fill="purple")
+# points = [250,0,500,500,0,500]
+# canvas.create_polygon(points, fill="yellow", outline="black", width=5)
+# canvas.create_arc(0,0,500,500, style=PIESLICE, fill="green", start=90, extent=180)
+canvas.create_arc(0,0,500,500, fill="red", extent=180, width=10)
+canvas.create_arc(0,0,500,500, fill="white", extent=180, start=180, width=10)
+canvas.create_oval(190,190,310,310, fill="white", width=10)
 
-percentLabel = Label(window, textvariable=percent).pack()
-
-taskLabel = Label(window, textvariable=text).pack()
 
 
-button = Button(window, text="download", command=start).pack()
 #######################################
-
+canvas.pack()
 window.mainloop()
