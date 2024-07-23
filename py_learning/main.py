06c974c74dc4a816a989059478bd8263788e9cc2
mym <maiyongming929@gmail.com>
2024-07-23 12:43:20 -0500

    py #88: mouse events

--- py_learning/main.py
+++ py_learning/main.py
@@ -5,16 +5,18 @@ window = Tk()
 
 #######################################
 def do(event):
-    # print("did " + event.keysym)
-    label.config(text=event.keysym)
+    print("did " + str(event.x) + " " + str(event.y))
 
 #######################################
-# window.bind("<Key>", do)
-window.bind("<Key>", do)
-label = Label(window, font=("Helvetica", 100))
-
+# window.bind("<Button-1>", do)
+# window.bind("<Button-2>", do)
+# window.bind("<Button-3>", do)
+# window.bind("<ButtonRelease>", do) 
+# window.bind("<Enter>", do) 
+# window.bind("<Leave>", do) 
+window.bind("<Motion>", do) 
 
 #######################################
 
-label.pack()
+
 window.mainloop()
