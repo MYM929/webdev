a50f826070a37c0c53a905a50595f58a19543207
mym <maiyongming929@gmail.com>
2024-07-23 12:38:49 -0500

    py #87: keyboard events

--- py_learning/main.py
+++ py_learning/main.py
@@ -4,23 +4,17 @@ from tkinter import *
 window = Tk()
 
 #######################################
+def do(event):
+    # print("did " + event.keysym)
+    label.config(text=event.keysym)
+
+#######################################
+# window.bind("<Key>", do)
+window.bind("<Key>", do)
+label = Label(window, font=("Helvetica", 100))
 
 
 #######################################
-canvas = Canvas(window, height=500, width=500)
 
-# canvas.create_line(0,0,500,500, fill="blue", width=5)
-# canvas.create_line(0,500,500,0, fill="red", width=5)
-# canvas.create_rectangle(50,50,250,250, fill="purple")
-# points = [250,0,500,500,0,500]
-# canvas.create_polygon(points, fill="yellow", outline="black", width=5)
-# canvas.create_arc(0,0,500,500, style=PIESLICE, fill="green", start=90, extent=180)
-canvas.create_arc(0,0,500,500, fill="red", extent=180, width=10)
-canvas.create_arc(0,0,500,500, fill="white", extent=180, start=180, width=10)
-canvas.create_oval(190,190,310,310, fill="white", width=10)
-
-
-
-#######################################
-canvas.pack()
+label.pack()
 window.mainloop()
