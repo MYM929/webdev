ae463ae88b754b9b089e5344d6aa536bca634ce8
mym <maiyongming929@gmail.com>
2024-07-25 12:40:49 -0500

    py #89: drag & drop

--- py_learning/main.py
+++ py_learning/main.py
@@ -4,17 +4,29 @@ from tkinter import *
 window = Tk()
 
 #######################################
-def do(event):
-    print("did " + str(event.x) + " " + str(event.y))
+def drag(event):
+    widget = event.widget
+    widget.startX = event.x
+    widget.startY = event.y
+
+def motion(event):
+    widget = event.widget
+    x = widget.winfo_x() - widget.startX + event.x
+    y = widget.winfo_y() - widget.startY + event.y
+    widget.place(x=x, y=y)
 
 #######################################
-# window.bind("<Button-1>", do)
-# window.bind("<Button-2>", do)
-# window.bind("<Button-3>", do)
-# window.bind("<ButtonRelease>", do) 
-# window.bind("<Enter>", do) 
-# window.bind("<Leave>", do) 
-window.bind("<Motion>", do) 
+label = Label(window, bg="red", width=10, height=5)
+label.place(x=0, y=0)
+
+label.bind("<Button-1>", drag)
+label.bind("<B1-Motion>", motion)
+
+label2 = Label(window, bg="blue", width=10, height=5)
+label2.place(x=100, y=0)
+
+label2.bind("<Button-1>", drag)
+label2.bind("<B1-Motion>", motion)
 
 #######################################
 
