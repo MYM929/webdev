759c4176b16211eca5e1cf35997268388f559fe4
mym <maiyongming929@gmail.com>
2024-07-29 12:41:49 -0500

    py #90: move images with keys commit 2

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import os
 
 
 window = Tk()
@@ -8,9 +9,13 @@ window = Tk()
 #######################################
 window.geometry("500x500")
 
-myImage = PhotoImage(file='image.jpg')
+myImage = PhotoImage(file='py_learning/image.png')
 label = Label(window, image=myImage)
 label.place(x=0,y=0)
+
+print(os.listdir())
+
+
 
 #######################################
 
