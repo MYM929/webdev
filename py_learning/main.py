e600283e299526687d006dd29266515a09229fab
mym <maiyongming929@gmail.com>
2024-07-31 12:42:30 -0500

    py #90: move images with keys commit 3

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,23 +1,47 @@
 from tkinter import *
+from PIL import Image, ImageTk
 import os
-
 
 window = Tk()
 
 #######################################
+def move_up(event):
+    label.place(x=label.winfo_x(), y=label.winfo_y()-10)
+def move_down(event):
+    label.place(x=label.winfo_x(), y=label.winfo_y()+10)
+def move_left(event):
+    label.place(x=label.winfo_x()-10, y=label.winfo_y())
+def move_right(event):
+    label.place(x=label.winfo_x()+10, y=label.winfo_y())
+
 
 #######################################
-window.geometry("500x500")
 
-myImage = PhotoImage(file='py_learning/image.png')
+window.geometry("1000x1000")
+
+window.bind("<w>", move_up)
+window.bind("<s>", move_down)
+window.bind("<a>", move_left)
+window.bind("<d>", move_right)
+
+
+
+
+# Load the image using Pillow
+original_image = Image.open('py_learning/image.png')
+
+# Scale the image to 20%
+width, height = original_image.size
+scaled_image = original_image.resize((int(width * 0.2), int(height * 0.2)))
+
+# Convert the image to PhotoImage
+myImage = ImageTk.PhotoImage(scaled_image)
+
 label = Label(window, image=myImage)
-label.place(x=0,y=0)
+label.place(x=0, y=0)
 
-print(os.listdir())
-
-
+# print(os.listdir())
 
 #######################################
-
 
 window.mainloop()
