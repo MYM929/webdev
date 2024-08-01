f5cf40451d37216511377fb7dc2d454d63cda1ab
mym <maiyongming929@gmail.com>
2024-08-01 12:20:18 -0500

    py #90: move images with keys final commit

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,30 +1,32 @@
 from tkinter import *
 from PIL import Image, ImageTk
-import os
 
 window = Tk()
 
-#######################################
+########################################
 def move_up(event):
-    label.place(x=label.winfo_x(), y=label.winfo_y()-10)
+    canvas.move(imageCanvas, 0, -10)
 def move_down(event):
-    label.place(x=label.winfo_x(), y=label.winfo_y()+10)
+    canvas.move(imageCanvas, 0, 10)
 def move_left(event):
-    label.place(x=label.winfo_x()-10, y=label.winfo_y())
+    canvas.move(imageCanvas, -10, 0)
 def move_right(event):
-    label.place(x=label.winfo_x()+10, y=label.winfo_y())
+    canvas.move(imageCanvas, 10, 0)
 
 
-#######################################
-
-window.geometry("1000x1000")
+########################################
 
 window.bind("<w>", move_up)
 window.bind("<s>", move_down)
 window.bind("<a>", move_left)
 window.bind("<d>", move_right)
+window.bind("<Up>", move_up)
+window.bind("<Down>", move_down)
+window.bind("<Left>", move_left)
+window.bind("<Right>", move_right)
 
-
+#######################################
+canvas = Canvas(window, width=500, height=500)
 
 
 # Load the image using Pillow
@@ -37,11 +39,66 @@ scaled_image = original_image.resize((int(width * 0.2), int(height * 0.2)))
 # Convert the image to PhotoImage
 myImage = ImageTk.PhotoImage(scaled_image)
 
-label = Label(window, image=myImage)
-label.place(x=0, y=0)
+imageCanvas = canvas.create_image(0,0,image=myImage, anchor=NW)
 
-# print(os.listdir())
 
 #######################################
-
+canvas.pack()
 window.mainloop()
+
+
+
+
+
+
+# from tkinter import *
+# from PIL import Image, ImageTk
+# import os
+
+# window = Tk()
+
+# #######################################
+# def move_up(event):
+#     label.place(x=label.winfo_x(), y=label.winfo_y()-10)
+# def move_down(event):
+#     label.place(x=label.winfo_x(), y=label.winfo_y()+10)
+# def move_left(event):
+#     label.place(x=label.winfo_x()-10, y=label.winfo_y())
+# def move_right(event):
+#     label.place(x=label.winfo_x()+10, y=label.winfo_y())
+
+
+# #######################################
+
+# window.geometry("1000x1000")
+
+# window.bind("<w>", move_up)
+# window.bind("<s>", move_down)
+# window.bind("<a>", move_left)
+# window.bind("<d>", move_right)
+# window.bind("<Up>", move_up)
+# window.bind("<Down>", move_down)
+# window.bind("<Left>", move_left)
+# window.bind("<Right>", move_right)
+
+
+
+
+# # Load the image using Pillow
+# original_image = Image.open('py_learning/image.png')
+
+# # Scale the image to 20%
+# width, height = original_image.size
+# scaled_image = original_image.resize((int(width * 0.2), int(height * 0.2)))
+
+# # Convert the image to PhotoImage
+# myImage = ImageTk.PhotoImage(scaled_image)
+
+# label = Label(window, image=myImage)
+# label.place(x=0, y=0)
+
+# # print(os.listdir())
+
+# #######################################
+
+# window.mainloop()
