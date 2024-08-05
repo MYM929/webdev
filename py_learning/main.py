8dbcd6a93e2b4fcb1cb76fd78e37a15e7cd0ff5f
mym <maiyongming929@gmail.com>
2024-08-05 12:38:51 -0500

    py #92: multiple animations

--- py_learning/main.py
+++ py_learning/main.py
@@ -1,51 +1,26 @@
 from tkinter import *
-from PIL import Image, ImageTk
 import time
-
-
-WIDTH = 500
-HEIGHT = 500
-xVelocity = 3
-yVelocity = 2
-
+from Ball import *
 
 window = Tk()
 
-#######################################
+WIDTH = 500
+HEIGHT = 500
 
 canvas = Canvas(window, width=WIDTH, height=HEIGHT)
-
-
-
-# Load the image using Pillow
-original_image = Image.open('py_learning/image.png')
-
-# Scale the image to 20%
-width, height = original_image.size
-scaled_image = original_image.resize((int(width * 0.2), int(height * 0.2)))
-
-# Convert the image to PhotoImage
-myImage = ImageTk.PhotoImage(scaled_image)
-
-imageCanvas = canvas.create_image(0, 0, image=myImage, anchor=NW)
-
 canvas.pack()
 
-image_width = myImage.width()
-image_height = myImage.height()
+volley_ball = Ball(canvas, 0, 0, 100, 1, 1, "white")
+tennis_ball = Ball(canvas, 0, 0, 50, 4, 3, "yellow")
+backet_ball = Ball(canvas, 0, 0, 125, 8, 7, "orange")
 
 while True:
-    coordinates = canvas.coords(imageCanvas)
-    print(coordinates)
-    if(coordinates[0] >= WIDTH-image_width or coordinates[0] < 0):
-        xVelocity = -xVelocity
-    if(coordinates[1] >= HEIGHT-image_height or coordinates[1] < 0):
-        yVelocity = -yVelocity
-    canvas.move(imageCanvas, xVelocity, yVelocity)
+    volley_ball.move()
+    tennis_ball.move()
+    backet_ball.move()
     window.update()
     time.sleep(0.01)
 
 
-#######################################
-
 window.mainloop()
+
